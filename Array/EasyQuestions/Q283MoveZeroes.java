c8a4c2b32df1430a9f65a36f1b30f4f747ff4dff
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-15 10:42:21 +0530

    Added Q283MoveZeroes code

--- Array/EasyQuestions/Q283MoveZeroes.java
+++ Array/EasyQuestions/Q283MoveZeroes.java
@@ -1,5 +1,14 @@
 package Array.EasyQuestions;
 
+/*
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order 
+of the non-zero elements.
+Note that you must do this in-place without making a copy of the array.
+
+Example 1:
+
+*/
+
 public class Q283MoveZeroes{
     
 }

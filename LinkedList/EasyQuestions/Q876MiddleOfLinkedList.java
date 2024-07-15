e489968bf88116799a802dd61afa98c0b5ad7745
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-15 16:18:42 +0530

    Added Q876MiddleOfLinkedList code

--- LinkedList/EasyQuestions/Q876MiddleOfLinkedList.java
+++ LinkedList/EasyQuestions/Q876MiddleOfLinkedList.java
@@ -1,5 +1,13 @@
 package LinkedList.EasyQuestions;
 
+/*
+Given the head of a singly linked list, return the middle node of the linked list.
+If there are two middle nodes, return the second middle node.
+
+Example 1:
+
+*/
+
 public class Q876MiddleOfLinkedList {
     
 }

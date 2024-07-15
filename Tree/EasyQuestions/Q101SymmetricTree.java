d5d0e01e93b5d201a82288022b87831580662329
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-15 16:51:50 +0530

    Added Q101SymmetricTree code

--- Tree/EasyQuestions/Q101SymmetricTree.java
+++ Tree/EasyQuestions/Q101SymmetricTree.java
@@ -3,8 +3,6 @@ package Tree.EasyQuestions;
 import java.util.LinkedList;
 import java.util.Queue;
 
-import javax.swing.tree.TreeNode;
-
 /*
 Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
 
@@ -18,26 +16,27 @@ Output: false
 */
 
 public class Q101SymmetricTree {
-    
+
     public boolean isSymmetric(TreeNode root) {
+
         Queue<TreeNode> queue = new LinkedList<>();
-        //add left and right node, ignoring the head
+        // add left and right node, ignoring the head
         queue.offer(root.left);
         queue.offer(root.right);
 
-        while(!queue.isEmpty()){
+        while (!queue.isEmpty()) {
 
             TreeNode left = queue.poll();
             TreeNode right = queue.poll();
 
-            if(left == null && right == null)
+            if (left == null && right == null)
                 continue;
-            if(left == null || right == null)
+            if (left == null || right == null)
                 return false;
-            if(left.val != right.val)
+            if (left.val != right.val)
                 return false;
-            
-            //otherwise add the next nodes smartly to compare the next time
+
+            // otherwise add the next nodes smartly to compare the next time
             queue.offer(left.left);
             queue.offer(right.right);
             queue.offer(left.right);
@@ -45,5 +44,24 @@ public class Q101SymmetricTree {
         }
         return true;
 
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
     }
 }

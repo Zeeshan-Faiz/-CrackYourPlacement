7fc315fd229fae850e733115c3ac3be6050cf0c1
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-16 10:55:31 +0530

    Added Q141LinkedListCycle code

--- LinkedList/EasyQuestions/Q141LinkedListCycle.java
+++ LinkedList/EasyQuestions/Q141LinkedListCycle.java
@@ -28,9 +28,9 @@ Explanation: There is no cycle in the linked list.
 public class Q141LinkedListCycle {
 
     public boolean hasCycle(ListNode head) {
+        
         ListNode fast = head;
         ListNode slow = head;
-
         while (fast != null && fast.next != null) {
             fast = fast.next.next;
             slow = slow.next;

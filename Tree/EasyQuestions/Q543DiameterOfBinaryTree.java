5186e7254c9ab7342a26fafa85d6f26c3af049e8
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-16 11:13:21 +0530

    Added Q543DiameterOfBinaryTree code

--- Tree/EasyQuestions/Q543DiameterOfBinaryTree.java
+++ Tree/EasyQuestions/Q543DiameterOfBinaryTree.java
@@ -35,4 +35,21 @@ public class Q543DiameterOfBinaryTree {
 
         return Math.max(left, right) + 1;
     }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+        TreeNode(int val) {
+            this.val = val;
+        }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }

8d758f4094f408f1392638594977a6db4845eef7
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-16 10:40:54 +0530

    Added Q28FindIndexOfFirstOccurence code

--- String/EasyQuestions/Q28FindIndexOfFirstOccurence.java
+++ String/EasyQuestions/Q28FindIndexOfFirstOccurence.java
@@ -1,5 +1,13 @@
 package String.EasyQuestions;
 
+/*
+Given two strings needle and haystack, return the index of the first occurrence of needle in 
+haystack, or -1 if needle is not part of haystack.
+
+Example 1:
+
+*/
+
 public class Q28FindIndexOfFirstOccurence {
     
 }

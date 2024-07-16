ac99db4196d389a0c9feb28e8029077906eac567
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-16 11:12:41 +0530

    Added Q543DiameterOfBinaryTree code

--- Tree/EasyQuestions/Q543DiameterOfBinaryTree.java
+++ Tree/EasyQuestions/Q543DiameterOfBinaryTree.java
@@ -18,4 +18,21 @@ Output: 1
 
 public class Q543DiameterOfBinaryTree {
     
+    int diameter = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        helper(root);
+        return diameter;
+    }
+
+    private int helper(TreeNode root) {
+        
+        if (root == null) {
+            return 0;
+        }
+        int left = helper(root.left);
+        int right = helper(root.right);
+        diameter = Math.max(diameter, left + right);
+        
+        return Math.max(left, right) + 1;
+    }
 }

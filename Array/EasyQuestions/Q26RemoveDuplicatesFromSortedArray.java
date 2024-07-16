e99d891d91e9a20f17260dd4ee8de6f9029121ee
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-16 10:09:13 +0530

    Added Q26RemoveDuplicatesFromSortedArray code

--- Array/EasyQuestions/Q26RemoveDuplicatesFromSortedArray.java
+++ Array/EasyQuestions/Q26RemoveDuplicatesFromSortedArray.java
@@ -25,4 +25,14 @@ It does not matter what you leave beyond the returned k (hence they are undersco
 
 public class Q26RemoveDuplicatesFromSortedArray {
     
+    public int removeDuplicates(int[] nums) {
+        int rd = 0;
+        for(int i = 1; i < nums.length ; i++){
+            if(nums[rd] != nums[i]){
+                rd++;
+                nums[rd] = nums[i];
+            }
+        }
+        return rd+1;
+    }
 }

930eb8d2368969f2214252372605019a47b9a18c
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-17 11:56:12 +0530

    Added Q104MaxDepthOfBinaryTree code

--- Tree/EasyQuestions/Q104MaxDepthOfBinaryTree.java
+++ Tree/EasyQuestions/Q104MaxDepthOfBinaryTree.java
@@ -1,5 +1,14 @@
 package Tree.EasyQuestions;
 
+/*
+Given the root of a binary tree, return its maximum depth.
+A binary tree's maximum depth is the number of nodes along the longest path from the root node 
+down to the farthest leaf node.
+
+Example 1:
+
+*/
+
 public class Q104MaxDepthOfBinaryTree {
     
 }

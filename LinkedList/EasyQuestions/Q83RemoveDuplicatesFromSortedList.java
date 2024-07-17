9cefce60fb094ea8ba3f48ed6514ae31a2a17b6f
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-17 11:42:32 +0530

    Added Q83RemoveDuplicatesFromSortedList code

--- LinkedList/EasyQuestions/Q83RemoveDuplicatesFromSortedList.java
+++ LinkedList/EasyQuestions/Q83RemoveDuplicatesFromSortedList.java
@@ -1,5 +1,13 @@
 package LinkedList.EasyQuestions;
 
+/*
+Given the head of a sorted linked list, delete all duplicates such that each element appears 
+only once. Return the linked list sorted as well.
+
+Example 1:
+
+*/
+
 public class Q83RemoveDuplicatesFromSortedList {
     
 }

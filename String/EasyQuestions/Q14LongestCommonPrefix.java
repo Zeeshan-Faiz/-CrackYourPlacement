fc6aec2e1c520615c42baf72568edad829aa3792
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-17 11:04:44 +0530

    Added Q14LongestCommonPrefix code

--- String/EasyQuestions/Q14LongestCommonPrefix.java
+++ String/EasyQuestions/Q14LongestCommonPrefix.java
@@ -1,5 +1,13 @@
 package String.EasyQuestions;
 
+/*
+Write a function to find the longest common prefix string amongst an array of strings.
+If there is no common prefix, return an empty string "".
+
+Example 1:
+
+*/
+
 public class Q14LongestCommonPrefix {
     
 }

8691f088e183c3549ac5177a5bcf1a36cce9e8ee
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-17 10:43:20 +0530

    Added Q1TwoSum code

--- Array/EasyQuestions/Q1TwoSum.java
+++ Array/EasyQuestions/Q1TwoSum.java
@@ -24,15 +24,16 @@ Output: [0,1]
 */
 
 public class Q1TwoSum {
-    
+
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int i = 0; i < nums.length; i++){
-            if(map.containsKey(target - nums[i]))
-                return new int[] {map.get(target - nums[i]), i};
-            
-            //or else add the current value in map
-            map.put(nums[i],i);
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i]))
+                return new int[] { map.get(target - nums[i]), i };
+
+            // or else add the current value in map
+            map.put(nums[i], i);
         }
         return new int[] {};
     }

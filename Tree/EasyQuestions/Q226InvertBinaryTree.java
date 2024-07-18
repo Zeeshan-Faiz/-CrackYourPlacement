789367cc0c0b52213a7bef073ac5cfdb458e753a
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-18 18:25:52 +0530

    Added Q226InvertBinaryTree code

--- Tree/EasyQuestions/Q226InvertBinaryTree.java
+++ Tree/EasyQuestions/Q226InvertBinaryTree.java
@@ -18,4 +18,18 @@ Output: []
 
 public class Q226InvertBinaryTree {
     
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+           return null;
+       }
+
+       TreeNode left = invertTree(root.left);
+       TreeNode right = invertTree(root.right);
+
+       root.left = right;
+       root.right = left;
+
+       return root;
+      
+   }
 }

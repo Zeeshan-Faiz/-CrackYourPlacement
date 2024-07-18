18c4c1293ed458f139258708010ec76dae4652a8
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-18 16:11:58 +0530

    Added Q689ValidPalindromeII code

--- String/EasyQuestions/Q689ValidPalindromeII.java
+++ String/EasyQuestions/Q689ValidPalindromeII.java
@@ -5,7 +5,17 @@ Given a string s, return true if the s can be palindrome after deleting at most 
 from it.
 
 Example 1:
+Input: s = "aba"
+Output: true
 
+Example 2:
+Input: s = "abca"
+Output: true
+Explanation: You could delete the character 'c'.
+
+Example 3:
+Input: s = "abc"
+Output: false
 */
 
 public class Q689ValidPalindromeII {

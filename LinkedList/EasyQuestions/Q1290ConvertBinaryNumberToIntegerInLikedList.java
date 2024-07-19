7d990e8177db6d7ff8ad69c2c69610aca52c04a9
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-19 18:25:49 +0530

    Added Q1290ConvertBinaryNumberToIntegerInLikedList

--- LinkedList/EasyQuestions/Q1290ConvertBinaryNumberToIntegerInLikedList.java
+++ LinkedList/EasyQuestions/Q1290ConvertBinaryNumberToIntegerInLikedList.java
@@ -6,7 +6,13 @@ linked list is either 0 or 1. The linked list holds the binary representation of
 Return the decimal value of the number in the linked list. The most significant bit is at the head of the linked list.
 
 Example 1:
+Input: head = [1,0,1]
+Output: 5
+Explanation: (101) in base 2 = (5) in base 10
 
+Example 2:
+Input: head = [0]
+Output: 0
 */
 
 public class Q1290ConvertBinaryNumberToIntegerInLikedList {

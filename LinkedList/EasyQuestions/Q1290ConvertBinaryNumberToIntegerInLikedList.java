cd38701cb436dfcd48e0dbd3600d2d18d63f7713
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-19 18:27:10 +0530

    Added Q1290ConvertBinaryNumberToIntegerInLikedList

--- LinkedList/EasyQuestions/Q1290ConvertBinaryNumberToIntegerInLikedList.java
+++ LinkedList/EasyQuestions/Q1290ConvertBinaryNumberToIntegerInLikedList.java
@@ -1,5 +1,7 @@
 package LinkedList.EasyQuestions;
 
+import LinkedList.EasyQuestions.Q876MiddleOfLinkedList.ListNode;
+
 /*
 Given head which is a reference node to a singly-linked list. The value of each node in the 
 linked list is either 0 or 1. The linked list holds the binary representation of a number.
@@ -16,12 +18,13 @@ Output: 0
 */
 
 public class Q1290ConvertBinaryNumberToIntegerInLikedList {
-    
+
     public int getDecimalValue(ListNode head) {
+
         int ans = 0;
-        while(head != null){
-            ans *= 2;
-            ans += head.val;
+        while (head != null) {
+            ans = ans * 2;
+            ans = ans + head.val;
             head = head.next;
         }
         return ans;

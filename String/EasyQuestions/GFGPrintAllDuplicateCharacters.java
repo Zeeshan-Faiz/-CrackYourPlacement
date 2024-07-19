6da3ebafb93c63de5e5f86f7a18dd8cecfb0da75
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-19 15:50:43 +0530

    Added GFGPrintAllDuplicateCharacters code

--- String/EasyQuestions/GFGPrintAllDuplicateCharacters.java
+++ String/EasyQuestions/GFGPrintAllDuplicateCharacters.java
@@ -4,7 +4,12 @@ package String.EasyQuestions;
 Given a string S, the task is to print all the duplicate characters with their occurrences in the given string.
 
 Example 1:
-
+Input: S = “geeksforgeeks”
+Output:
+e, count = 4
+g, count = 2
+k, count = 2
+s, count = 2 
 */
 
 public class GFGPrintAllDuplicateCharacters {

b5a54a4ef6f18e8232aaddfd2e5879f8f5d8ca82
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-19 15:51:49 +0530

    Added GFGPrintAllDuplicateCharacters code

--- String/EasyQuestions/GFGPrintAllDuplicateCharacters.java
+++ String/EasyQuestions/GFGPrintAllDuplicateCharacters.java
@@ -16,20 +16,23 @@ s, count = 2
 */
 
 public class GFGPrintAllDuplicateCharacters {
-    
-    static void printDups(String str)
-  {
-    Map<Character, Integer> count = new HashMap<>();
-    for (int i = 0; i < str.length(); i++) {
-      if(count.containsKey(str.charAt(i)))
-        count.put(str.charAt(i) , count.get(str.charAt(i))+1); 
-      else count.put(str.charAt(i),1);
-      //increase the count of characters by 1 
-    }
 
-    for (Map.Entry<Character,Integer> mapElement : count.entrySet()) {   //iterating through the unordered map 
-      if (mapElement.getValue() > 1)   //if the count of characters is greater than 1 then duplicate found
-        System.out.println(mapElement.getKey() + ", count = " + mapElement.getValue());
+    static void printDups(String str) {
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        //count the frequency of each character
+        for (int i = 0; i < str.length(); i++) {
+            if (map.containsKey(str.charAt(i)))
+                map.put(str.charAt(i), map.get(str.charAt(i)) + 1);
+            else
+                map.put(str.charAt(i), 1);
+        }
+
+        for (Map.Entry<Character, Integer> mapElement : map.entrySet()) 
+        {
+            if (mapElement.getValue() > 1)
+                System.out.println(mapElement.getKey() + ", count = " + mapElement.getValue());
+        }
     }
-  }
 }

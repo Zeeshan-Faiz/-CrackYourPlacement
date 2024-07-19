29bd02013ab81d94b9ac3b05c2773969abba1d72
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-19 19:17:31 +0530

    Added Q572SubtreeOfAnotherTree code

--- Tree/EasyQuestions/Q572SubtreeOfAnotherTree.java
+++ Tree/EasyQuestions/Q572SubtreeOfAnotherTree.java
@@ -18,7 +18,7 @@ Output: false
 */
 
 public class Q572SubtreeOfAnotherTree {
- 
+
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
 
         if (root == null)
@@ -26,5 +26,16 @@ public class Q572SubtreeOfAnotherTree {
         if (isSame(root, subRoot))
             return true;
         return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    public boolean isSame(TreeNode root, TreeNode subRoot) {
+
+        if (root == null && subRoot == null)
+            return true;
+        if (root == null || subRoot == null)
+            return false;
+        if (root.val != subRoot.val)
+            return false;
+        return isSame(root.left, subRoot.left) && isSame(root.right, subRoot.right);
     }
 }

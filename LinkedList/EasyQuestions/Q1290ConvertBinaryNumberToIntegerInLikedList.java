b82d13feddeda159d81c1e56b6152ae56804a47c
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-19 18:26:47 +0530

    Added Q1290ConvertBinaryNumberToIntegerInLikedList

--- LinkedList/EasyQuestions/Q1290ConvertBinaryNumberToIntegerInLikedList.java
+++ LinkedList/EasyQuestions/Q1290ConvertBinaryNumberToIntegerInLikedList.java
@@ -17,4 +17,13 @@ Output: 0
 
 public class Q1290ConvertBinaryNumberToIntegerInLikedList {
     
+    public int getDecimalValue(ListNode head) {
+        int ans = 0;
+        while(head != null){
+            ans *= 2;
+            ans += head.val;
+            head = head.next;
+        }
+        return ans;
+    }
 }

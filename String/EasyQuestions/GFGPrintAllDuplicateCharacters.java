50eb96251592ca391e412823f49c494ff474131f
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-19 15:50:56 +0530

    Added GFGPrintAllDuplicateCharacters code

--- String/EasyQuestions/GFGPrintAllDuplicateCharacters.java
+++ String/EasyQuestions/GFGPrintAllDuplicateCharacters.java
@@ -1,5 +1,8 @@
 package String.EasyQuestions;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /*
 Given a string S, the task is to print all the duplicate characters with their occurrences in the given string.
 
@@ -14,4 +17,19 @@ s, count = 2
 
 public class GFGPrintAllDuplicateCharacters {
     
+    static void printDups(String str)
+  {
+    Map<Character, Integer> count = new HashMap<>();
+    for (int i = 0; i < str.length(); i++) {
+      if(count.containsKey(str.charAt(i)))
+        count.put(str.charAt(i) , count.get(str.charAt(i))+1); 
+      else count.put(str.charAt(i),1);
+      //increase the count of characters by 1 
+    }
+
+    for (Map.Entry<Character,Integer> mapElement : count.entrySet()) {   //iterating through the unordered map 
+      if (mapElement.getValue() > 1)   //if the count of characters is greater than 1 then duplicate found
+        System.out.println(mapElement.getKey() + ", count = " + mapElement.getValue());
+    }
+  }
 }

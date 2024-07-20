7fc0d627c50a14146816c768cae2b91e626ad456
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-20 13:57:14 +0530

    Added Q67AddBinary code

--- Mathematical/EasyQuestions/Q67AddBinary.java
+++ Mathematical/EasyQuestions/Q67AddBinary.java
@@ -14,4 +14,28 @@ Output: "10101"
 
 public class Q67AddBinary {
     
+    public String addBinary(String a, String b) {
+        
+        StringBuilder res = new StringBuilder();
+        int carry = 0;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+
+        while (i >= 0 || j >= 0 || carry == 1) {
+            if (i >= 0) {
+                carry = carry + a.charAt(i) - '0';
+                i--;
+            }
+
+            if (j >= 0) {
+                carry = carry + b.charAt(j) - '0';
+                j--;
+            }
+
+            //add the carry into the res
+            res.append(carry % 2);
+            carry = carry / 2;
+        }
+        return res.reverse().toString();
+    }
 }

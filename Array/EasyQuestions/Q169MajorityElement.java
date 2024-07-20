dac396b2e9aa3493fa6d4d3b2657636de53bfa48
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-20 13:22:07 +0530

    Added Q169MajorityElement code

--- Array/EasyQuestions/Q169MajorityElement.java
+++ Array/EasyQuestions/Q169MajorityElement.java
@@ -1,5 +1,14 @@
 package Array.EasyQuestions;
 
+/*
+Given an array nums of size n, return the majority element.
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the 
+majority element always exists in the array.
+
+Example 1:
+
+*/
+
 public class Q169MajorityElement {
     
 }

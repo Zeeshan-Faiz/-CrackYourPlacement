e507f520d1d8da17546d89baea6120e7e85c8fb4
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-20 13:22:18 +0530

    Added Q169MajorityElement code

--- Array/EasyQuestions/Q169MajorityElement.java
+++ Array/EasyQuestions/Q169MajorityElement.java
@@ -6,7 +6,12 @@ The majority element is the element that appears more than ⌊n / 2⌋ times. Yo
 majority element always exists in the array.
 
 Example 1:
+Input: nums = [3,2,3]
+Output: 3
 
+Example 2:
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
 */
 
 public class Q169MajorityElement {

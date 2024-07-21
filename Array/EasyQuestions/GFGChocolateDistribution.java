af6c0e2de9d9572a7e3094883c3c2b72efcbb63d
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-21 15:29:58 +0530

    Added GFGChocolateDistribution code

--- Array/EasyQuestions/GFGChocolateDistribution.java
+++ Array/EasyQuestions/GFGChocolateDistribution.java
@@ -8,7 +8,16 @@ students, the task is to distribute chocolate packets among M students such that
     2. The difference between maximum number of chocolates given to a student and minimum number of chocolates given to a student is minimum.
 
 Example 1:
+N = 8, M = 5
+A = {3, 4, 1, 9, 56, 7, 9, 12}
+Output: 6
+Explanation: The minimum difference between maximum chocolates and minimum chocolates is 9 - 3 = 6 by choosing following M packets :{3, 4, 9, 7, 9}.
 
+Example 2:
+N = 7, M = 3
+A = {7, 3, 2, 4, 9, 12, 56}
+Output: 2
+Explanation: The minimum difference between maximum chocolates and minimum chocolates is 4 - 2 = 2 by choosing following M packets :{3, 2, 4}.
 */
 
 public class GFGChocolateDistribution {

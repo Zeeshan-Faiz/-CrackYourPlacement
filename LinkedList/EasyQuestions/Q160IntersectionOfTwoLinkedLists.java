e1eacce8753284725ce3159dfe1c82a1e4ab59f9
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-21 19:57:19 +0530

    Added Q160IntersectionOfTwoLinkedLists code

--- LinkedList/EasyQuestions/Q160IntersectionOfTwoLinkedLists.java
+++ LinkedList/EasyQuestions/Q160IntersectionOfTwoLinkedLists.java
@@ -1,5 +1,13 @@
 package LinkedList.EasyQuestions;
 
+/*
+Given the heads of two singly linked-lists headA and headB, return the node at which the two 
+lists intersect. If the two linked lists have no intersection at all, return null.
+
+Example 1:
+
+*/
+
 public class Q160IntersectionOfTwoLinkedLists {
     
 }

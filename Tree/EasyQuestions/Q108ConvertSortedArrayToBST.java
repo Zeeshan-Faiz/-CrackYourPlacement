d26e2716df799a3859b7ea111b475919655d24c0
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-21 20:13:35 +0530

    Added Q108ConvertSortedArrayToBST code

--- Tree/EasyQuestions/Q108ConvertSortedArrayToBST.java
+++ Tree/EasyQuestions/Q108ConvertSortedArrayToBST.java
@@ -22,4 +22,23 @@ public class Q108ConvertSortedArrayToBST {
             return null;
         return CreateBST(nums, 0, nums.length - 1);
     }
+
+    private TreeNode CreateBST(int nums[], int l, int r) {
+
+        if (l > r)
+            return null;
+
+        // we take middle element as head value such that it will become height balanced
+        // tree
+        int mid = l + (r - l) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+
+        // assign the value for left of subtree that is l to mid -1 for given array
+        root.left = CreateBST(nums, l, mid - 1);
+
+        // assign the value for right go subtree that is mid+1 to r for given array
+        root.right = CreateBST(nums, mid + 1, r);
+
+        return root;
+    }
 }

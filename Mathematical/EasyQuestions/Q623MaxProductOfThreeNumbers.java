9c8208105084e825e6ea1a2b281008dd7748e401
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-21 15:54:41 +0530

    Added Q623MaxProductOfThreeNumbers code

--- Mathematical/EasyQuestions/Q623MaxProductOfThreeNumbers.java
+++ Mathematical/EasyQuestions/Q623MaxProductOfThreeNumbers.java
@@ -19,4 +19,45 @@ Output: -6
 
 public class Q623MaxProductOfThreeNumbers {
     
+    public int maximumProduct(int[] nums) {
+        
+        int maxNum1 = Integer.MIN_VALUE;
+        int maxNum2 = Integer.MIN_VALUE;
+        int maxNum3 = Integer.MIN_VALUE; // largest value
+        int minNum1 = Integer.MAX_VALUE;
+        int minNum2 = Integer.MAX_VALUE;
+         
+        for(int num : nums) {
+            //Find three largest numbers.
+            if(num > maxNum1){
+                if(num > maxNum3 ) {
+                    maxNum1 = maxNum2;
+                    maxNum2 = maxNum3;
+                    maxNum3 = num;
+                } 
+                else if(num > maxNum2) {
+                   maxNum1 = maxNum2;
+                   maxNum2 = num;
+                } 
+                else {
+                    maxNum1 = num;
+                }
+
+            }
+            
+            // Find two smallest numbers.
+            if(num < minNum2){
+                if(num < minNum1) {
+                   minNum2 = minNum1;
+                   minNum1 = num;
+                } else {
+                    minNum2 = num;
+                }
+            }
+        }
+        
+        int maxProduct = maxNum1 * maxNum2 * maxNum3;
+        int minProduct = minNum1 * minNum2 * maxNum3;
+        return Math.max(minProduct, maxProduct);
+    }
 }

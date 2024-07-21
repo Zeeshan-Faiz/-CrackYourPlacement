c096dc02859e30156be8c1d9a0e37dac952fc5e1
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-21 15:54:18 +0530

    Added Q623MaxProductOfThreeNumbers code

--- Mathematical/EasyQuestions/Q623MaxProductOfThreeNumbers.java
+++ Mathematical/EasyQuestions/Q623MaxProductOfThreeNumbers.java
@@ -1,5 +1,13 @@
 package Mathematical.EasyQuestions;
 
+/*
+Given an integer array nums, find three numbers whose product is maximum and return the 
+maximum product.
+
+Example 1:
+
+*/
+
 public class Q623MaxProductOfThreeNumbers {
     
 }

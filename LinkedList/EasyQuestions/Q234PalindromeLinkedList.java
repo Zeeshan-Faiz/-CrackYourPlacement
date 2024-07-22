7446c20155c7ee43967455f0acd0a7416edaf3e5
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-22 19:46:53 +0530

    Added Q234PalindromeLinkedList code

--- LinkedList/EasyQuestions/Q234PalindromeLinkedList.java
+++ LinkedList/EasyQuestions/Q234PalindromeLinkedList.java
@@ -1,5 +1,12 @@
 package LinkedList.EasyQuestions;
 
+/*
+Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+
+Example 1:
+
+*/
+
 public class Q234PalindromeLinkedList {
     
 }

401f494291b4e9842614e11b4fdad19093dea137
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-22 20:09:05 +0530

    Added Q617MergeTwoBinaryTrees code

--- Tree/EasyQuestions/Q617MergeTwoBinaryTrees.java
+++ Tree/EasyQuestions/Q617MergeTwoBinaryTrees.java
@@ -1,5 +1,7 @@
 package Tree.EasyQuestions;
 
+import Tree.EasyQuestions.Q101SymmetricTree.TreeNode;
+
 /*
 You are given two binary trees root1 and root2.
 Imagine that when you put one of them to cover the other, some nodes of the two trees are 
@@ -19,18 +21,17 @@ Output: [2,2]
 */
 
 public class Q617MergeTwoBinaryTrees {
-    
+
     public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        
         if (root1 == null && root2 == null)
             return null;
-
         if (root1 == null)
             return root2;
-
         if (root2 == null)
             return root1;
 
-        //if both node exist at same point then we will sum the value of both nodes & store in root1.
+        // if both node exist at same point then we will sum the value of both nodes & store in root1.
         root1.val = root1.val + root2.val;
         root1.left = mergeTrees(root1.left, root2.left);
         root1.right = mergeTrees(root1.right, root2.right);

49b58b192760bb89876b16c74ffb9a400fb26178
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-22 20:08:31 +0530

    Added Q617MergeTwoBinaryTrees code

--- Tree/EasyQuestions/Q617MergeTwoBinaryTrees.java
+++ Tree/EasyQuestions/Q617MergeTwoBinaryTrees.java
@@ -10,7 +10,12 @@ Return the merged tree.
 Note: The merging process must start from the root nodes of both trees.
 
 Example 1:
+Input: root1 = [1,3,2,5], root2 = [2,1,3,null,4,null,7]
+Output: [3,4,5,5,4,null,7]
 
+Example 2:
+Input: root1 = [1], root2 = [1,2]
+Output: [2,2]
 */
 
 public class Q617MergeTwoBinaryTrees {

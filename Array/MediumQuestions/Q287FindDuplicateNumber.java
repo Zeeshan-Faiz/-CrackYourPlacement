1bcd6503b3a1d25dc3f9651f60cba35778ca0ca7
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-22 10:21:36 +0530

    Added Q287FindDuplicateNumber code

--- Array/MediumQuestions/Q287FindDuplicateNumber.java
+++ Array/MediumQuestions/Q287FindDuplicateNumber.java
@@ -1,5 +1,14 @@
 package Array.MediumQuestions;
 
+/*
+Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+There is only one repeated number in nums, return this repeated number.
+You must solve the problem without modifying the array nums and uses only constant extra space.
+
+Example 1:
+
+*/
+
 public class Q287FindDuplicateNumber {
     
 }

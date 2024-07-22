00698c8ea3b9aa553ac59bda10dcf206b891f398
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-22 10:47:20 +0530

    Added Q168ExcelSheetColumnTitle code

--- Mathematical/EasyQuestions/Q168ExcelSheetColumnTitle.java
+++ Mathematical/EasyQuestions/Q168ExcelSheetColumnTitle.java
@@ -27,4 +27,13 @@ Output: "ZY"
 
 public class Q168ExcelSheetColumnTitle {
     
+    public String convertToTitle(int col) {
+        StringBuilder ans = new StringBuilder();
+        while (col > 0) {
+            int r = (col - 1) % 26;
+            ans.append((char)(r + 'A'));
+            col = (col - 1) / 26;
+        }
+        return ans.reverse().toString();
+    }
 }

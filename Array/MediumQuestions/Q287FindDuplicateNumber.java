47d78cd1cc61f208f9433e1fa53c4885f6e8aa58
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-22 10:22:06 +0530

    Added Q287FindDuplicateNumber code

--- Array/MediumQuestions/Q287FindDuplicateNumber.java
+++ Array/MediumQuestions/Q287FindDuplicateNumber.java
@@ -20,4 +20,22 @@ Output: 3
 
 public class Q287FindDuplicateNumber {
     
+    public int findDuplicate(int[] nums) {
+        int slow = nums[nums[0]];
+        int fast = nums[nums[nums[0]]];
+
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+
+        slow = nums[0];
+
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
 }

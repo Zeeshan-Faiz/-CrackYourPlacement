f4f9fbe1670c34cd03c91243763ae4fca78c3ec3
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-23 10:53:56 +0530

    Added Q462MinMovesToEqualArrayElementsII code

--- Mathematical/MediumQuestions/Q462MinMovesToEqualArrayElementsII.java
+++ Mathematical/MediumQuestions/Q462MinMovesToEqualArrayElementsII.java
@@ -6,7 +6,15 @@ array elements equal. In one move, you can increment or decrement an element of 
 Test cases are designed so that the answer will fit in a 32-bit integer.
 
 Example 1:
+Input: nums = [1,2,3]
+Output: 2
+Explanation:
+Only two moves are needed (remember each move increments or decrements one element):
+[1,2,3]  =>  [2,2,3]  =>  [2,2,2]
 
+Example 2:
+Input: nums = [1,10,2,9]
+Output: 16
 */
 
 public class Q462MinMovesToEqualArrayElementsII {

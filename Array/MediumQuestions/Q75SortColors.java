9264517b12110212ca12a9e7ace78ae21d9e99d7
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-23 10:32:11 +0530

    Added Q75SortColors code

--- Array/MediumQuestions/Q75SortColors.java
+++ Array/MediumQuestions/Q75SortColors.java
@@ -16,5 +16,21 @@ Output: [0,1,2]
 */
 
 public class Q75SortColors {
-    
+ 
+    public void sortColors(int[] nums) {
+        int low = 0, mid = 0, high = nums.length - 1;
+        while(mid <= high){
+            if(nums[mid] == 0){
+                swap(nums,low,mid);
+                low++;
+                mid++;
+            }
+            else if(nums[mid] == 1)
+                mid++;
+            else if(nums[mid] == 2){
+                swap(nums,mid,high);
+                high--;
+            }
+        }
+    }
 }

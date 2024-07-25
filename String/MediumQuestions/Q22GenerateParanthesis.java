4da3594b456202f97f814b337510f466eb169658
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-25 10:12:39 +0530

    Added Q22GenerateParanthesis code

--- String/MediumQuestions/Q22GenerateParanthesis.java
+++ String/MediumQuestions/Q22GenerateParanthesis.java
@@ -17,10 +17,27 @@ Output: ["()"]
 */
 
 public class Q22GenerateParanthesis {
-    
+
     public List<String> generateParenthesis(int n) {
+        
         List<String> result = new ArrayList<>();
         helper(result, new char[2 * n], 0, 0, 0, n);
         return result;
+    }
+
+    public void helper(List<String> result, char[] array, int open, int close, int index, int n) {
+
+        if (index == array.length) {
+            result.add(new String(array));
+            return;
+        }
+        if (open < n) {
+            array[index] = '(';
+            helper(result, array, open + 1, close, index + 1, n);
+        }
+        if (close < open) {
+            array[index] = ')';
+            helper(result, array, open, close + 1, index + 1, n);
+        }
     }
 }

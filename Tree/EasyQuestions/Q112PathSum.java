b7f9337cfb03bcccdd0eb44a88fa4cff8f3e5a7f
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-25 22:36:54 +0530

    Added Q112PathSum code

--- Tree/EasyQuestions/Q112PathSum.java
+++ Tree/EasyQuestions/Q112PathSum.java
@@ -1,5 +1,14 @@
 package Tree.EasyQuestions;
 
+/*
+Given the root of a binary tree and an integer targetSum, return true if the tree has a 
+root-to-leaf path such that adding up all the values along the path equals targetSum.
+A leaf is a node with no children.
+
+Example 1:
+
+*/
+
 public class Q112PathSum {
     
 }

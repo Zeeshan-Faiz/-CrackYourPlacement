20504c09ecbc4370cc0b0bd471336b3a36633827
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-25 09:45:21 +0530

    Added Q442FindAllDuplicatesInArray code

--- Array/MediumQuestions/Q442FindAllDuplicatesInArray.java
+++ Array/MediumQuestions/Q442FindAllDuplicatesInArray.java
@@ -1,5 +1,8 @@
 package Array.MediumQuestions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
 Given an integer array nums of length n where all the integers of nums are in the range [1, n]
 and each integer appears once or twice, return an array of all the integers that appears twice.
@@ -19,5 +22,28 @@ Output: []
 */
 
 public class Q442FindAllDuplicatesInArray {
-    
+
+    /*
+     * We can either use Cyclic Sort or the below approach as the elements lies in
+     * [1,n] range
+     */
+
+    public List<Integer> findDuplicates(int[] nums) {
+
+        List<Integer> resultSet = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            // Get the index, the element corresponds to
+            int index = Math.abs(nums[i]) - 1;
+
+            // If the number is already negative, it means we are
+            // encountering it twice
+            if (nums[index] < 0)
+                resultSet.add(index + 1);
+
+            // Flip the number at the index to negative
+            nums[index] = nums[index] * -1;
+        }
+
+        return resultSet;
+    }
 }

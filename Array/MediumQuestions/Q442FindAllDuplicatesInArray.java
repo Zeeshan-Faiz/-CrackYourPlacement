5350aaa3174d2905b57c301097ce855d6b2df99c
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-25 09:45:45 +0530

    Added Q442FindAllDuplicatesInArray code

--- Array/MediumQuestions/Q442FindAllDuplicatesInArray.java
+++ Array/MediumQuestions/Q442FindAllDuplicatesInArray.java
@@ -31,19 +31,18 @@ public class Q442FindAllDuplicatesInArray {
     public List<Integer> findDuplicates(int[] nums) {
 
         List<Integer> resultSet = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) 
+        {    
             // Get the index, the element corresponds to
             int index = Math.abs(nums[i]) - 1;
 
-            // If the number is already negative, it means we are
-            // encountering it twice
+            // If the number is already negative, it means we are encountering it twice
             if (nums[index] < 0)
                 resultSet.add(index + 1);
 
             // Flip the number at the index to negative
             nums[index] = nums[index] * -1;
         }
-
         return resultSet;
     }
 }

e38c5c357ae7f09a865b1d858d2efcc7ed9b6f3f
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-26 22:26:23 +0530

    Added Q530MinAbsoluteDiffInBST code

--- Tree/EasyQuestions/Q530MinAbsoluteDiffInBST.java
+++ Tree/EasyQuestions/Q530MinAbsoluteDiffInBST.java
@@ -5,9 +5,38 @@ Given the root of a Binary Search Tree (BST), return the minimum absolute differ
 the values of any two different nodes in the tree.
 
 Example 1:
+Input: root = [4,2,6,1,3]
+Output: 1
 
+Example 2:
+Input: root = [1,0,48,null,null,12,49]
+Output: 1
 */
 
 public class Q530MinAbsoluteDiffInBST {
     
+    Integer prev = null;
+    int minDiff = Integer.MAX_VALUE;
+
+    public int getMinimumDifference(TreeNode root) {
+        if (root == null) {
+            return minDiff;
+        }
+
+        //in order traversal to compare all nodes
+
+        // Traverse the left subtree
+        getMinimumDifference(root.left);
+
+        // Update minDiff and prev value when not at the first node
+        if (prev != null) {
+            minDiff = Math.min(minDiff, Math.abs(root.val - prev));
+        }
+        prev = root.val;
+
+        // Traverse the right subtree
+        getMinimumDifference(root.right);
+
+        return minDiff;
+    }
 }

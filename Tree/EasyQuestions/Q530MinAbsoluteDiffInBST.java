f9a0ec7c4ca272a85fcc5e969a69c6acc6418df5
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-26 22:25:57 +0530

    Added Q530MinAbsoluteDiffInBST code

--- Tree/EasyQuestions/Q530MinAbsoluteDiffInBST.java
+++ Tree/EasyQuestions/Q530MinAbsoluteDiffInBST.java
@@ -1,5 +1,13 @@
 package Tree.EasyQuestions;
 
+/*
+Given the root of a Binary Search Tree (BST), return the minimum absolute difference between 
+the values of any two different nodes in the tree.
+
+Example 1:
+
+*/
+
 public class Q530MinAbsoluteDiffInBST {
     
 }

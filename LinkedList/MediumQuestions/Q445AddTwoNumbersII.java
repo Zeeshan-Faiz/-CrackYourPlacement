a07d97cbae08c50609fe32ffb3e9c325674e31ce
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-26 19:56:22 +0530

    Added Q445AddTwoNumbersII code

--- LinkedList/MediumQuestions/Q445AddTwoNumbersII.java
+++ LinkedList/MediumQuestions/Q445AddTwoNumbersII.java
@@ -7,7 +7,16 @@ numbers and return the sum as a linked list.
 You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
 Example 1:
+Input: l1 = [7,2,4,3], l2 = [5,6,4]
+Output: [7,8,0,7]
 
+Example 2:
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [8,0,7]
+
+Example 3:
+Input: l1 = [0], l2 = [0]
+Output: [0]
 */
 
 public class Q445AddTwoNumbersII {

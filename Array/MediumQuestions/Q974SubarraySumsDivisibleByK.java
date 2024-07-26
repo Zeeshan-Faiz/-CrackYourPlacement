619b3a5735192215d1cc39eca2976a42c99c389d
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-26 18:21:52 +0530

    Added Q974SubarraySumsDivisibleByK code

--- Array/MediumQuestions/Q974SubarraySumsDivisibleByK.java
+++ Array/MediumQuestions/Q974SubarraySumsDivisibleByK.java
@@ -1,5 +1,13 @@
 package Array.MediumQuestions;
 
+/*
+Given an integer array nums and an integer k, return the number of non-empty subarrays that 
+have a sum divisible by k. A subarray is a contiguous part of an array.
+
+Example 1:
+
+*/
+
 public class Q974SubarraySumsDivisibleByK {
     
 }

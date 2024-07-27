62a7fceac38017f472052d26bbcb20b6e7bd6d16
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-27 19:12:14 +0530

    Added Q151RevereseWordsInAString code

--- String/MediumQuestions/Q151RevereseWordsInAString.java
+++ String/MediumQuestions/Q151RevereseWordsInAString.java
@@ -9,7 +9,18 @@ The returned string should only have a single space separating the words. Do not
 extra spaces.
 
 Example 1:
+Input: s = "the sky is blue"
+Output: "blue is sky the"
 
+Example 2:
+Input: s = "  hello world  "
+Output: "world hello"
+Explanation: Your reversed string should not contain leading or trailing spaces.
+
+Example 3:
+Input: s = "a good   example"
+Output: "example good a"
+Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
 */
 
 public class Q151RevereseWordsInAString {

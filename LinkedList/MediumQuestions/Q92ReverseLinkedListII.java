8ef8919bdd635dcaf6f17a2d8e39587e63df923b
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-27 19:46:44 +0530

    Added Q92ReverseLinkedListII code

--- LinkedList/MediumQuestions/Q92ReverseLinkedListII.java
+++ LinkedList/MediumQuestions/Q92ReverseLinkedListII.java
@@ -15,4 +15,30 @@ Output: [5]
 
 public class Q92ReverseLinkedListII {
     
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        
+        if (head == null || left == right) {
+            return head;
+        }
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+
+        // skip left-1 nodes
+        for (int i = 0; i < left - 1; i++) {
+            prev = prev.next;
+        }
+
+        ListNode cur = prev.next;
+        //reverse in between left and right
+        for (int i = 0; i < right - left; i++) {
+            ListNode temp = cur.next;
+            cur.next = temp.next;
+            temp.next = prev.next;
+            prev.next = temp;
+        }
+
+        return dummy.next;
+    }
 }

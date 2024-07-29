b5b667a9a79eac8911975b9977fa4c3f236f30df
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-29 20:32:16 +0530

    Added Q122BestTimeToSellStockII code

--- Array/MediumQuestions/Q122BestTimeToSellStockII.java
+++ Array/MediumQuestions/Q122BestTimeToSellStockII.java
@@ -26,13 +26,14 @@ Explanation: There is no way to make a positive profit, so we never buy the stoc
 */
 
 public class Q122BestTimeToSellStockII {
-    
+
     public int maxProfit(int[] prices) {
+        
         int profit = 0;
-        for(int i = 1; i < prices.length; i++){
-             
-            if(prices[i] > prices[i-1])
-                profit = profit + (prices[i] - prices[i-1]);
+        for (int i = 1; i < prices.length; i++) {
+
+            if (prices[i] > prices[i - 1])
+                profit = profit + (prices[i] - prices[i - 1]);
         }
         return profit;
     }

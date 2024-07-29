48ac9611cf7e37b7c30bcdaafa44e122b1816e01
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-29 22:28:43 +0530

    Added Q110BalancedBinaryTree code

--- Tree/EasyQuestions/Q110BalancedBinaryTree.java
+++ Tree/EasyQuestions/Q110BalancedBinaryTree.java
@@ -6,7 +6,16 @@ A height-balanced binary tree is a binary tree in which the depth of the two sub
 node never differs by more than one.
 
 Example 1:
+Input: root = [3,9,20,null,null,15,7]
+Output: true
 
+Example 2:
+Input: root = [1,2,2,3,3,null,null,4,4]
+Output: false
+
+Example 3:
+Input: root = []
+Output: true.
 */
 
 public class Q110BalancedBinaryTree {

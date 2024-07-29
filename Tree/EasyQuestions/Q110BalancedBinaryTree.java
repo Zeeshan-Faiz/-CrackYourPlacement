6e37fa63c33b94aadd79ba5416eb674d734e69bb
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-29 22:27:57 +0530

    Added Q110BalancedBinaryTree code

--- Tree/EasyQuestions/Q110BalancedBinaryTree.java
+++ Tree/EasyQuestions/Q110BalancedBinaryTree.java
@@ -1,5 +1,14 @@
 package Tree.EasyQuestions;
 
+/*
+Given a binary tree, determine if it is height-balanced.
+A height-balanced binary tree is a binary tree in which the depth of the two subtrees of every \
+node never differs by more than one.
+
+Example 1:
+
+*/
+
 public class Q110BalancedBinaryTree {
     
 }

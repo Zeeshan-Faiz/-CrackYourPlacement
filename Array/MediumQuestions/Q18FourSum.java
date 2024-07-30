99d52789480b4a7d15394c627823f43e6459a4ce
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-30 08:55:23 +0530

    Added Q18FourSum code

--- Array/MediumQuestions/Q18FourSum.java
+++ Array/MediumQuestions/Q18FourSum.java
@@ -23,29 +23,28 @@ Output: [[2,2,2,2]]
 */
 
 public class Q18FourSum {
-    
+
     public List<List<Integer>> fourSum(int[] nums, int target) {
+        
         int n = nums.length;
         List<List<Integer>> ans = new ArrayList<>();
-
-        // sort the given array:
         Arrays.sort(nums);
 
         for (int i = 0; i < n; i++) 
         {
-            // avoid the duplicates while moving i:
+            // avoid the duplicates for i:
             if (i > 0 && nums[i] == nums[i - 1])
                 continue;
             for (int j = i + 1; j < n; j++) 
             {
-                // avoid the duplicates while moving j:
+                // avoid the duplicates for j:
                 if (j > i + 1 && nums[j] == nums[j - 1])
                     continue;
 
-                // 2 pointers:
                 int k = j + 1;
                 int l = n - 1;
-                while (k < l) {
+                while (k < l) 
+                {
                     long sum = nums[i];
                     sum += nums[j];
                     sum += nums[k];
@@ -60,19 +59,19 @@ public class Q18FourSum {
                         k++;
                         l--;
 
-                        // skip the duplicates:
+                        // skip the duplicates for k & l:
                         while (k < l && nums[k] == nums[k - 1])
                             k++;
                         while (k < l && nums[l] == nums[l + 1])
                             l--;
-                    } else if (sum < target)
+                    } 
+                    else if (sum < target)
                         k++;
                     else
                         l--;
                 }
             }
         }
-
         return ans;
     }
 }

d7d4991f7d8732531ee9e64a71a776d9cdd56988
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-07-30 10:00:29 +0530

    Added Q227BasicCalculatorII code

--- String/MediumQuestions/Q227BasicCalculatorII.java
+++ String/MediumQuestions/Q227BasicCalculatorII.java
@@ -21,5 +21,42 @@ Output: 5
 */
 
 public class Q227BasicCalculatorII {
-    
+
+    public int calculate(String s) {
+        
+        int num = 0, last = 0, sum = 0;
+        char operator = '+';
+
+        for (int i = 0; i < s.length(); i++) 
+        {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                num = num * 10 + (c - '0');
+            }
+
+            if (isOperator(c) || i == s.length() - 1) 
+            {
+                if (operator == '+') {
+                    sum += last;
+                    last = num;
+                } 
+                else if (operator == '-') {
+                    sum += last;
+                    last = -num;
+                } 
+                else if (operator == '*')
+                    last *= num;
+                else if (operator == '/')
+                    last /= num;
+
+                num = 0;
+                operator = c;
+            }
+        }
+        return sum += last;
+    }
+
+    private boolean isOperator(char c) {
+        return c == '+' || c == '-' || c == '*' || c == '/';
+    }
 }

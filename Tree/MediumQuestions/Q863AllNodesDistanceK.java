9bf1fba78e25c97d7429070629b2bb8f1f94ef6a
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-01 19:41:42 +0530

    Added Q863AllNodesDistanceK code

--- Tree/MediumQuestions/Q863AllNodesDistanceK.java
+++ Tree/MediumQuestions/Q863AllNodesDistanceK.java
@@ -1,6 +1,9 @@
 package Tree.MediumQuestions;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 
@@ -20,6 +23,50 @@ Output: []
 */
 
 public class Q863AllNodesDistanceK {
+
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+
+        Map<TreeNode, TreeNode> map = new HashMap<>();
+        setparent(root, map);
+        Map<TreeNode, Boolean> check = new HashMap<>();
+        check.put(target, true);
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(target);
+        int level = 0;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            if (level == k) {
+                break;
+            }
+            level++;
+
+            for (int i = 0; i < size; i++) {
+                TreeNode temp = q.poll();
+                if (temp.left != null && check.get(temp.left) == null) {
+                    q.add(temp.left);
+                    check.put(temp.left, true);
+
+                }
+                if (temp.right != null && check.get(temp.right) == null) {
+                    q.add(temp.right);
+                    check.put(temp.right, true);
+                }
+                if (map.get(temp) != null && check.get(map.get(temp)) == null) {
+                    q.add(map.get(temp));
+                    check.put(map.get(temp), true);
+
+                }
+
+            }
+        }
+        List<Integer> mc = new ArrayList<>();
+        while (!q.isEmpty()) {
+            TreeNode help = q.poll();
+            mc.add(help.val);
+        }
+
+        return mc;
+    }
 
     public void setparent(TreeNode root, Map<TreeNode, TreeNode> map) {
         Queue<TreeNode> q = new LinkedList<>();

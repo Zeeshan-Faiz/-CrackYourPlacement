dd1773041694c9f35a5f6dd923186f976de254e5
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-01 11:07:32 +0530

    Added Q148SortList code

--- LinkedList/MediumQuestions/Q148SortList.java
+++ LinkedList/MediumQuestions/Q148SortList.java
@@ -1,5 +1,21 @@
 package LinkedList.MediumQuestions;
 
+/*
+Given the head of a linked list, return the list after sorting it in ascending order.
+
+Example 1:
+Input: head = [4,2,1,3]
+Output: [1,2,3,4]
+
+Example 2:
+Input: head = [-1,5,3,4,0]
+Output: [-1,0,3,4,5]
+
+Example 3:
+Input: head = []
+Output: []
+*/
+
 public class Q148SortList {
     
 }

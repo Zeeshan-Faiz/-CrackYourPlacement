c16fcd03b532394a9531643c5ed551d63535b2a2
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-01 11:08:52 +0530

    Added Q148SortList code

--- LinkedList/MediumQuestions/Q148SortList.java
+++ LinkedList/MediumQuestions/Q148SortList.java
@@ -69,4 +69,20 @@ public class Q148SortList {
 
         return dummy.next;
     }
+
+    public class ListNode {
+        
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+        ListNode(int val) {
+            this.val = val;
+        }
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
 }

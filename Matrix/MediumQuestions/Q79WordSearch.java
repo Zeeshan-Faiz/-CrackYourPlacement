1dca8078229cd9ed093248c78ad732ddb99c8d2c
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-02 12:15:55 +0530

    Added Q79WordSearch code

--- Matrix/MediumQuestions/Q79WordSearch.java
+++ Matrix/MediumQuestions/Q79WordSearch.java
@@ -7,7 +7,16 @@ adjacent cells are horizontally or vertically neighboring. The same letter cell 
 used more than once.
 
 Example 1:
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+Output: true
 
+Example 2:
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
+Output: true
+
+Example 3:
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
+Output: false
 */
 
 public class Q79WordSearch {

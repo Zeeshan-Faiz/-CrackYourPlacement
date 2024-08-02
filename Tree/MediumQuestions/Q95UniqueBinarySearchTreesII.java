14468196c8ca49b2fb00319570162345b4cd1dc3
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-02 16:16:06 +0530

    Added Q95UniqueBinarySearchTreesII code

--- Tree/MediumQuestions/Q95UniqueBinarySearchTreesII.java
+++ Tree/MediumQuestions/Q95UniqueBinarySearchTreesII.java
@@ -22,6 +22,29 @@ public class Q95UniqueBinarySearchTreesII {
 
         if (n <= 0)
             return new ArrayList<>();
-        return generate_trees(1, n);
+        return helper(1, n);
+    }
+
+    private List<TreeNode> helper(int start, int end) {
+        List<TreeNode> all_trees = new ArrayList<>();
+        if (start > end) {
+            all_trees.add(null);
+            return all_trees;
+        }
+
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> left_trees = helper(start, i - 1);
+            List<TreeNode> right_trees = helper(i + 1, end);
+
+            for (TreeNode l : left_trees) {
+                for (TreeNode r : right_trees) {
+                    TreeNode current_tree = new TreeNode(i);
+                    current_tree.left = l;
+                    current_tree.right = r;
+                    all_trees.add(current_tree);
+                }
+            }
+        }
+        return all_trees;
     }
 }

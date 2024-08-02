0fe8a411aea37a833fc67e1c9ae055dbe2457b8d
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-02 14:10:57 +0530

    Added Q378KthSmallestElementInSortedMatrix code

--- Matrix/MediumQuestions/Q378KthSmallestElementInSortedMatrix.java
+++ Matrix/MediumQuestions/Q378KthSmallestElementInSortedMatrix.java
@@ -7,7 +7,13 @@ the sorted order, not the kth distinct element.
 You must find a solution with a memory complexity better than O(n2).
 
 Example 1:
+Input: matrix = [[1,5,9],[10,11,13],[12,13,15]], k = 8
+Output: 13
+Explanation: The elements in the matrix are [1,5,9,10,11,12,13,13,15], and the 8th smallest number is 13
 
+Example 2:
+Input: matrix = [[-5]], k = 1
+Output: -5
 */
 
 public class Q378KthSmallestElementInSortedMatrix {

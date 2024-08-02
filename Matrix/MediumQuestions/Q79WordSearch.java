2b2cff7232ab27cda13ba8f6595e384952bd7244
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-02 12:16:24 +0530

    Added Q79WordSearch code

--- Matrix/MediumQuestions/Q79WordSearch.java
+++ Matrix/MediumQuestions/Q79WordSearch.java
@@ -22,7 +22,7 @@ Output: false
 public class Q79WordSearch {
 
     public boolean exist(char[][] board, String word) {
-        
+
         char[] words = word.toCharArray();
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
@@ -31,5 +31,25 @@ public class Q79WordSearch {
             }
         }
         return false;
+    }
+
+    private boolean helper(char[][] board, int i, int j, char[] words, int index) {
+
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length ||
+                board[i][j] == '*' || board[i][j] != words[index])
+            return false;
+
+        if (index == words.length - 1)
+            return true;
+
+        char ch = board[i][j];
+        board[i][j] = '*';// mark visited
+        boolean res = helper(board, i + 1, j, words, index + 1) ||
+                helper(board, i - 1, j, words, index + 1) ||
+                helper(board, i, j + 1, words, index + 1) ||
+                helper(board, i, j - 1, words, index + 1);
+
+        board[i][j] = ch; // backtrack and unmark the visited
+        return res;
     }
 }

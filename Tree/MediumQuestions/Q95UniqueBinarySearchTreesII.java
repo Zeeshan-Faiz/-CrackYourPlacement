7b74294015c1ef1ed02ac04a4301aadc12a241ad
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-02 16:15:21 +0530

    Added Q95UniqueBinarySearchTreesII code

--- Tree/MediumQuestions/Q95UniqueBinarySearchTreesII.java
+++ Tree/MediumQuestions/Q95UniqueBinarySearchTreesII.java
@@ -1,5 +1,13 @@
 package Tree.MediumQuestions;
 
+/*
+Given an integer n, return all the structurally unique BST's (binary search trees), which 
+has exactly n nodes of unique values from 1 to n. Return the answer in any order.
+
+Example 1:
+
+*/
+
 public class Q95UniqueBinarySearchTreesII {
     
 }

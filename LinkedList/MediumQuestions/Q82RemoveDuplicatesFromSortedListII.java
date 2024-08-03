54fdcd12ac761ee0b2ebe1345c0eea8f587d621a
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-03 16:56:01 +0530

    Added Q82RemoveDuplicatesFromSortedListII code

--- LinkedList/MediumQuestions/Q82RemoveDuplicatesFromSortedListII.java
+++ LinkedList/MediumQuestions/Q82RemoveDuplicatesFromSortedListII.java
@@ -41,4 +41,20 @@ public class Q82RemoveDuplicatesFromSortedListII {
         }
         return dummy.next;
     }
+
+    public class ListNode {
+        
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+        ListNode(int val) {
+            this.val = val;
+        }
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
 }

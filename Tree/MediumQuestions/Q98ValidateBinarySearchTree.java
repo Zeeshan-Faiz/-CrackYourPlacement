d605fc33baa71f8695a947775bce3a07b5a9df59
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-03 18:35:32 +0530

    Added Q98ValidateBinarySearchTree code

--- Tree/MediumQuestions/Q98ValidateBinarySearchTree.java
+++ Tree/MediumQuestions/Q98ValidateBinarySearchTree.java
@@ -34,4 +34,21 @@ public class Q98ValidateBinarySearchTree {
         
         return helper(root.left, minVal, root.val) && helper(root.right, root.val, maxVal);
     }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+        TreeNode(int val) {
+            this.val = val;
+        }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }

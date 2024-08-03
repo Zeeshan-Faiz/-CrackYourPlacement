3cf64166950684f63f5fd195c3c0a741423cb1c2
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-03 16:53:14 +0530

    Added Q82RemoveDuplicatesFromSortedListII code

--- LinkedList/MediumQuestions/Q82RemoveDuplicatesFromSortedListII.java
+++ LinkedList/MediumQuestions/Q82RemoveDuplicatesFromSortedListII.java
@@ -1,5 +1,13 @@
 package LinkedList.MediumQuestions;
 
+/*
+Given the head of a sorted linked list, delete all nodes that have duplicate numbers, 
+leaving only distinct numbers from the original list. Return the linked list sorted as well.
+
+Example 1:
+
+*/
+
 public class Q82RemoveDuplicatesFromSortedListII {
     
 }

c1705aa9016ea24c8a8c0a65c861c06a03b139b1
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-03 16:10:19 +0530

    Added Q264UglyNumberII code

--- HeapPriorityQueue/MediumQuestions/Q264UglyNumberII.java
+++ HeapPriorityQueue/MediumQuestions/Q264UglyNumberII.java
@@ -1,5 +1,13 @@
 package HeapPriorityQueue.MediumQuestions;
 
+/*
+An ugly number is a positive integer whose prime factors are limited to 2, 3, and 5.
+Given an integer n, return the nth ugly number.
+
+Example 1:
+
+*/
+
 public class Q264UglyNumberII {
     
 }

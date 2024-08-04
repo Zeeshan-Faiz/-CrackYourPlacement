1d6d2854b521a840d2a771fcbf3435eef46f8c01
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-04 11:43:28 +0530

    Added Q11ContainerWithMostWater code

--- Array/MediumQuestions/Q11ContainerWithMostWater.java
+++ Array/MediumQuestions/Q11ContainerWithMostWater.java
@@ -21,23 +21,20 @@ public class Q11ContainerWithMostWater {
     
     public int maxArea(int[] height) {
         
-        int left = 0;
-        int right = height.length - 1;
+        int left = 0, right = height.length - 1;
         int maxArea = 0;
 
         while (left < right) 
         {
+            // area = length of smaller stick * widhth of both sticks
             int currentArea = Math.min(height[left], height[right]) * (right - left);
             maxArea = Math.max(maxArea, currentArea);
 
-            if (height[left] < height[right]) {
+            if (height[left] < height[right])
                 left++;
-            } 
-            else {
+            else
                 right--;
-            }
         }
-
         return maxArea;
     }
 }

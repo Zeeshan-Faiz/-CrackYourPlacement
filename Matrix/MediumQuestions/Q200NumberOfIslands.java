97fb810dde19014956d1c7d0b0e11916918876b9
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-04 13:07:29 +0530

    Added Q200NumberOfIslands code

--- Matrix/MediumQuestions/Q200NumberOfIslands.java
+++ Matrix/MediumQuestions/Q200NumberOfIslands.java
@@ -27,4 +27,16 @@ Output: 3
 
 public class Q200NumberOfIslands {
     
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
 }

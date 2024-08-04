62876e5187cdd804fc9f68bfdbb5d27201e6baa8
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-04 12:18:13 +0530

    Added RabinKarpAlgorithm code

--- String/MediumQuestions/RabinKarpAlgorithm.java
+++ String/MediumQuestions/RabinKarpAlgorithm.java
@@ -1,5 +1,14 @@
 package String.MediumQuestions;
 
+/*
+Given a text T[0. . .n-1] and a pattern P[0. . .m-1], write a function 
+search(String text,String pattern) that prints all occurrences of pattern present in text using 
+Rabin Karp algorithm.
+The Rabin-Karp algorithm check every substring. But unlike the Naive algorithm, it matches the 
+hash value of the pattern with the hash value of the current substring of text, and if the 
+hash values match then only it starts matching individual characters.
+*/
+
 public class RabinKarpAlgorithm {
     
 }

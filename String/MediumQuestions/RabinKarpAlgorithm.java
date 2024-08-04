a27d02a374e59bbc5b7013666ead13ea99cc9506
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-04 12:18:27 +0530

    Added RabinKarpAlgorithm code

--- String/MediumQuestions/RabinKarpAlgorithm.java
+++ String/MediumQuestions/RabinKarpAlgorithm.java
@@ -11,4 +11,21 @@ hash values match then only it starts matching individual characters.
 
 public class RabinKarpAlgorithm {
     
+    public void search(String text, String pattern) {
+        int patternLength = pattern.length();
+          double patternHash = calculateHash(pattern);
+          double textHash = calculateHash(text.substring(0, patternLength));
+    
+        for(int i=0; i<= text.length() - patternLength; i++) {
+          if(textHash == patternHash) {
+            if(text.substring(i, i+patternLength).equals(pattern)) {
+              System.out.println("Pattern found at index " + i);
+            }
+          }
+    
+          if (i < text.length() - patternLength) {
+            textHash = updateHash(textHash, text.charAt(i), text.charAt(i + patternLength), patternLength);
+          }
+        }
+      }
 }

79b8388f6ae65129b90590569883f9dd8b2275b1
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-04 12:19:44 +0530

    Added RabinKarpAlgorithm code

--- String/MediumQuestions/RabinKarpAlgorithm.java
+++ String/MediumQuestions/RabinKarpAlgorithm.java
@@ -12,27 +12,26 @@ hash values match then only it starts matching individual characters.
 public class RabinKarpAlgorithm {
 
     private final int PRIME = 101;
+
     public void search(String text, String pattern) {
 
         int patternLength = pattern.length();
         double patternHash = calculateHash(pattern);
         double textHash = calculateHash(text.substring(0, patternLength));
 
-        for (int i = 0; i <= text.length() - patternLength; i++) {
+        for (int i = 0; i <= text.length() - patternLength; i++) 
+        {
             if (textHash == patternHash) {
-                if (text.substring(i, i + patternLength).equals(pattern)) {
+                if (text.substring(i, i + patternLength).equals(pattern))
                     System.out.println("Pattern found at index " + i);
-                }
             }
-
-            if (i < text.length() - patternLength) {
+            if (i < text.length() - patternLength)
                 textHash = updateHash(textHash, text.charAt(i), text.charAt(i + patternLength), patternLength);
-            }
         }
     }
 
     private double calculateHash(String str) {
-        
+
         double hash = 0;
         for (int i = 0; i < str.length(); i++) {
             hash += str.charAt(i) * Math.pow(PRIME, i);
@@ -41,8 +40,9 @@ public class RabinKarpAlgorithm {
     }
 
     private double updateHash(double prevHash, char oldChar, char newChar, int patternLength) {
+        
         double newHash = (prevHash - oldChar) / PRIME;
-      newHash = newHash + newChar * Math.pow(PRIME, patternLength - 1);
-      return newHash;
+        newHash = newHash + newChar * Math.pow(PRIME, patternLength - 1);
+        return newHash;
     }
 }

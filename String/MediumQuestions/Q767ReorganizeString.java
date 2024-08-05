a905084adaf77a430890127baeedefb7ebc18183
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-05 22:26:55 +0530

    Added Q767ReorganizeString code

--- String/MediumQuestions/Q767ReorganizeString.java
+++ String/MediumQuestions/Q767ReorganizeString.java
@@ -1,5 +1,13 @@
 package String.MediumQuestions;
 
+/*
+Given a string s, rearrange the characters of s so that any two adjacent characters are not the same.
+Return any possible rearrangement of s or return "" if not possible.
+
+Example 1:
+
+*/
+
 public class Q767ReorganizeString {
     
 }

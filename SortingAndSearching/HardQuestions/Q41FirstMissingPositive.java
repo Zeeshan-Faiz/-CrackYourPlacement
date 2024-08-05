f507668afa6afb673e7f76ce435153e25ca342c7
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-05 21:57:02 +0530

    Added Q41FirstMissingPositive code

--- SortingAndSearching/HardQuestions/Q41FirstMissingPositive.java
+++ SortingAndSearching/HardQuestions/Q41FirstMissingPositive.java
@@ -1,5 +1,13 @@
 package SortingAndSearching.HardQuestions;
 
+/*
+Given an unsorted integer array nums. Return the smallest positive integer that is not present in nums.
+You must implement an algorithm that runs in O(n) time and uses O(1) auxiliary space.
+
+Example 1:
+
+*/
+
 public class Q41FirstMissingPositive {
     
 }

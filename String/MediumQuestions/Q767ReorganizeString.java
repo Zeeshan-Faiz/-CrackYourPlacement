8bdbdffc0584cf96622d120f8c11a31447ba8ebe
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-05 22:28:54 +0530

    Added Q767ReorganizeString code

--- String/MediumQuestions/Q767ReorganizeString.java
+++ String/MediumQuestions/Q767ReorganizeString.java
@@ -17,16 +17,16 @@ public class Q767ReorganizeString {
 
     public String reorganizeString(String str) {
 
-        int[] hash = new int[26];
+        int[] freq = new int[26];
         // count frequency of each character
         for (int i = 0; i < str.length(); i++)
-            hash[str.charAt(i) - 'a']++;
+            freq[str.charAt(i) - 'a']++;
 
         // find the character which occured the maximum time
         int max = 0, letter = 0;
-        for (int i = 0; i < hash.length; i++) {
-            if (hash[i] > max) {
-                max = hash[i];
+        for (int i = 0; i < freq.length; i++) {
+            if (freq[i] > max) {
+                max = freq[i];
                 letter = i;
             }
         }
@@ -38,21 +38,21 @@ public class Q767ReorganizeString {
 
         // Fill all even places with majority character
         int idx = 0;
-        while (hash[letter] > 0) {
+        while (freq[letter] > 0) {
             res[idx] = (char) (letter + 'a');
             idx += 2;
-            hash[letter]--;
+            freq[letter]--;
         }
 
         // Fill the remaining characters
-        for (int i = 0; i < hash.length; i++) {
-            while (hash[i] > 0) {
+        for (int i = 0; i < freq.length; i++) {
+            while (freq[i] > 0) {
                 if (idx >= res.length)
                     idx = 1;
 
                 res[idx] = (char) (i + 'a');
                 idx += 2;
-                hash[i]--;
+                freq[i]--;
             }
         }
         return String.valueOf(res);

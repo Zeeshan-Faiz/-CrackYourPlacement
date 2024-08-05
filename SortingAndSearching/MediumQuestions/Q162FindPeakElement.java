64d6f9a0a5313b963d356b6e172dada8bc2046da
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-05 20:09:14 +0530

    Added Q162FindPeakElement code

--- SortingAndSearching/MediumQuestions/Q162FindPeakElement.java
+++ SortingAndSearching/MediumQuestions/Q162FindPeakElement.java
@@ -1,5 +1,17 @@
 package SortingAndSearching.MediumQuestions;
 
+/*
+A peak element is an element that is strictly greater than its neighbors.
+Given a 0-indexed integer array nums, find a peak element, and return its index. If the array 
+contains multiple peaks, return the index to any of the peaks.
+You may imagine that nums[-1] = nums[n] = -∞. In other words, an element is always considered 
+to be strictly greater than a neighbor that is outside the array.
+You must write an algorithm that runs in O(log n) time.
+
+Example 1:
+
+*/
+
 public class Q162FindPeakElement {
     
 }

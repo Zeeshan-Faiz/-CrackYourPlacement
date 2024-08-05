41ae64f1ca7455d315253acee37a2c18349b6509
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-05 10:46:14 +0530

    Added Q46Permutations code

--- Array/MediumQuestions/Q46Permutations.java
+++ Array/MediumQuestions/Q46Permutations.java
@@ -1,5 +1,13 @@
 package Array.MediumQuestions;
 
+/*
+Given an array nums of distinct integers, return all the possible permutations. You can 
+return the answer in any order.
+
+Example 1:
+
+*/
+
 public class Q46Permutations {
     
 }

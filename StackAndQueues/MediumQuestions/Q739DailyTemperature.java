510f699133e8589ef805d2ab24615978a73e578f
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-06 19:49:29 +0530

    Added Q739DailyTemperature code

--- StackAndQueues/MediumQuestions/Q739DailyTemperature.java
+++ StackAndQueues/MediumQuestions/Q739DailyTemperature.java
@@ -1,5 +1,7 @@
 package StackAndQueues.MediumQuestions;
 
+import java.util.Stack;
+
 /*
 Given an array of integers temperatures represents the daily temperatures, return an array 
 answer such that answer[i] is the number of days you have to wait after the ith day to get a 
@@ -21,4 +23,29 @@ Output: [1,1,0]
 
 public class Q739DailyTemperature {
     
+    Stack<Integer> helperStack = new Stack<>();
+
+        int n = temperatures.length;
+        int[] result = new int[n];
+
+        for (int idx = n - 1; idx >= 0; idx--) {
+
+            // Popping all indices with a lower or equal
+            // temperature than the current index
+            while (!helperStack.isEmpty()
+                    && temperatures[idx] >= temperatures[helperStack.peek()]) {
+                helperStack.pop();
+            }
+
+            // If the stack still has elements,
+            // then the next warmer temperature exists!
+            if (!helperStack.isEmpty()) {
+                result[idx] = helperStack.peek() - idx;
+            }
+
+            // Inserting current index in the stack
+            helperStack.push(idx);
+        }
+
+        return result;
 }

4ca5f66e8c828624f467066d2f6239a683e31782
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-06 10:27:27 +0530

    Added Q2006CountNoOfPairsWithAbsDiff code

--- Array/EasyQuestions/Q2006CountNoOfPairsWithAbsDiff.java
+++ Array/EasyQuestions/Q2006CountNoOfPairsWithAbsDiff.java
@@ -34,20 +34,21 @@ Explanation: The pairs with an absolute difference of 2 are:
 */
 
 public class Q2006CountNoOfPairsWithAbsDiff {
-    
+
     public int countKDifference(int[] nums, int k) {
-        Map<Integer,Integer> map = new HashMap<>();
-        int res = 0;
         
-        for(int i = 0;i< nums.length;i++){
-            if(map.containsKey(nums[i]-k)){
-                res+= map.get(nums[i]-k);
-            }
-            if(map.containsKey(nums[i]+k)){
-                res+= map.get(nums[i]+k);
-            }
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        Map<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) 
+        {
+            if (map.containsKey(nums[i] - k))
+                count += map.get(nums[i] - k);
+            if (map.containsKey(nums[i] + k))
+                count += map.get(nums[i] + k);
+            
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
         }
-        return res;
+        return count;
     }
 }

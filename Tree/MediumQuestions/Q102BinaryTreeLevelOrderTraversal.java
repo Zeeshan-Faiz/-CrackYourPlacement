30e319af6ac537e404f7af0c8449a5cd1bb7d33d
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-06 22:17:17 +0530

    Added Q102BinaryTreeLevelOrderTraversal code

--- Tree/MediumQuestions/Q102BinaryTreeLevelOrderTraversal.java
+++ Tree/MediumQuestions/Q102BinaryTreeLevelOrderTraversal.java
@@ -1,5 +1,10 @@
 package Tree.MediumQuestions;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 /*
 Given the root of a binary tree, return the level order traversal of its nodes' values. 
 (i.e., from left to right, level by level).
@@ -19,4 +24,29 @@ Output: []
 
 public class Q102BinaryTreeLevelOrderTraversal {
     
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        
+        List<List<Integer>> result = new ArrayList<>();
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        if(root == null)
+            return new ArrayList<>();
+        queue.offer(root);
+
+        while(!queue.isEmpty()){
+            int len = queue.size();
+            List<Integer> ans = new ArrayList<>();
+            for(int i = 0; i < len; i++){
+                TreeNode temp = queue.poll();
+                ans.add(temp.val);
+
+                if(temp.left != null)
+                    queue.add(temp.left);
+                if(temp.right != null)
+                    queue.add(temp.right);
+            }
+            result.add(ans);
+        }
+        return result;
+    }
 }

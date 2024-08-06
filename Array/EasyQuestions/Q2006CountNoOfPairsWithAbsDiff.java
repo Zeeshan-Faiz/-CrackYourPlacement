f664721a214d1a16204cd9690776e46ce02ca0a2
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-06 10:23:46 +0530

    Added Q2006CountNoOfPairsWithAbsDiff code

--- Array/EasyQuestions/Q2006CountNoOfPairsWithAbsDiff.java
+++ Array/EasyQuestions/Q2006CountNoOfPairsWithAbsDiff.java
@@ -1,5 +1,16 @@
 package Array.EasyQuestions;
 
+/*
+Given an integer array nums and an integer k, return the number of pairs (i, j) where i < j 
+such that |nums[i] - nums[j]| == k.
+The value of |x| is defined as:
+    x if x >= 0.
+    -x if x < 0.
+
+Example 1:
+
+*/
+
 public class Q2006CountNoOfPairsWithAbsDiff {
     
 }

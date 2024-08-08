2710805f034498bdda4737a083d5533acbd73e0a
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-08 10:05:40 +0530

    Added Q844BackspaceStringCompare code

--- StackAndQueues/EasyQuestions/Q844BackspaceStringCompare.java
+++ StackAndQueues/EasyQuestions/Q844BackspaceStringCompare.java
@@ -1,5 +1,13 @@
 package StackAndQueues.EasyQuestions;
 
+/*
+Given two strings s and t, return true if they are equal when both are typed into empty text 
+editors. '#' means a backspace character. Note that after backspacing an empty text, the text will continue empty.
+
+Example 1:
+
+*/
+
 public class Q844BackspaceStringCompare {
     
 }

283e9d086984040a69ed4c5830f4b0370d512ae9
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-08 10:06:26 +0530

    Added Q844BackspaceStringCompare code

--- StackAndQueues/EasyQuestions/Q844BackspaceStringCompare.java
+++ StackAndQueues/EasyQuestions/Q844BackspaceStringCompare.java
@@ -21,5 +21,33 @@ Explanation: s becomes "c" while t becomes "b".
 */
 
 public class Q844BackspaceStringCompare {
-    
+
+    public boolean backspaceCompare(String s, String t) {
+        return getActual(s).equals(getActual(t));
+    }
+
+    private String getActual(String input) {
+
+        StringBuilder actualString = new StringBuilder();
+        int hashCount = 0;
+
+        for (int i = input.length() - 1; i >= 0; i--) {
+
+            // Keep a count of backspace characters
+            if (input.charAt(i) == '#') {
+                hashCount++;
+                continue;
+            }
+
+            // If backspace count > 0 reduce it and skip the character
+            if (hashCount > 0) {
+                hashCount--;
+            } else {
+                // If no backspace, just insert at beginning
+                actualString.insert(0, input.charAt(i));
+            }
+        }
+
+        return actualString.toString();
+    }
 }

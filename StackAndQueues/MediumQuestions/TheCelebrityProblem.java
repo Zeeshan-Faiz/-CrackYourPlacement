a4762e2c5a7bc86c5713883c10112a0f955a7ae4
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-09 18:45:28 +0530

    Added TheCelebrityProblem code

--- StackAndQueues/MediumQuestions/TheCelebrityProblem.java
+++ StackAndQueues/MediumQuestions/TheCelebrityProblem.java
@@ -1,5 +1,16 @@
 package StackAndQueues.MediumQuestions;
 
+/*
+A celebrity is a person who is known to all but does not know anyone at a party. A party is 
+being organized by some people.  A square matrix mat is used to represent people at the party 
+such that if an element of row i and column j is set to 1 it means ith person knows jth person. 
+You need to return the index of the celebrity in the party, if the celebrity does not exist, 
+return -1.
+
+Example 1:
+
+*/
+
 public class TheCelebrityProblem {
     
 }

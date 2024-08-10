f0a1684708a3c9646e593957928546071632112a
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-10 19:24:39 +0530

    Added QMultiplyTwoNumbersInLinkedList code

--- LinkedList/EasyQuestions/QMultiplyTwoNumbersInLinkedList.java
+++ LinkedList/EasyQuestions/QMultiplyTwoNumbersInLinkedList.java
@@ -1,5 +1,13 @@
 package LinkedList.EasyQuestions;
 
+/*
+Given two numbers represented by linked lists, write a function that returns the 
+multiplication of these two linked lists.
+
+Example 1:
+
+*/
+
 public class QMultiplyTwoNumbersInLinkedList {
     
 }

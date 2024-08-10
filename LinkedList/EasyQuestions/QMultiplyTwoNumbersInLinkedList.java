7abb63a2629368a370671265445a070b3df3fd50
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-10 19:25:09 +0530

    Added QMultiplyTwoNumbersInLinkedList code

--- LinkedList/EasyQuestions/QMultiplyTwoNumbersInLinkedList.java
+++ LinkedList/EasyQuestions/QMultiplyTwoNumbersInLinkedList.java
@@ -1,5 +1,7 @@
 package LinkedList.EasyQuestions;
 
+import org.w3c.dom.Node;
+
 /*
 Given two numbers represented by linked lists, write a function that returns the 
 multiplication of these two linked lists.
@@ -15,5 +17,25 @@ Output : 3852
 */
 
 public class QMultiplyTwoNumbersInLinkedList {
-    
+
+    // Multiply contents of two linked lists
+    static long multiplyTwoLists(Node first, Node second) {
+        long N = 1000000007;
+        long num1 = 0, num2 = 0;
+
+        while (first != null || second != null) {
+
+            if (first != null) {
+                num1 = ((num1) * 10) % N + first.data;
+                first = first.next;
+            }
+
+            if (second != null) {
+                num2 = ((num2) * 10) % N + second.data;
+                second = second.next;
+            }
+
+        }
+        return ((num1 % N) * (num2 % N)) % N;
+    }
 }

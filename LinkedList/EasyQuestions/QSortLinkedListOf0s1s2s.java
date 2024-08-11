620a4bf13789496532ab81e61ddf6861f6299764
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-11 21:51:54 +0530

    Added QSortLinkedListOf0s1s2s code

--- LinkedList/EasyQuestions/QSortLinkedListOf0s1s2s.java
+++ LinkedList/EasyQuestions/QSortLinkedListOf0s1s2s.java
@@ -1,5 +1,12 @@
 package LinkedList.EasyQuestions;
 
+/*
+Given a linked list of 0s, 1s and 2s, The task is to sort and print it.
+
+Example 1:
+
+*/
+
 public class QSortLinkedListOf0s1s2s {
     
 }

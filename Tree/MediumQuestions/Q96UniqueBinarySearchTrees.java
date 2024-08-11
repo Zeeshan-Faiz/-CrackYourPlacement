f355e7720e21482d27e832dde250ca9bd2ac7906
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-11 22:30:40 +0530

    Added QSortLinkedListOf0s1s2s code

--- Tree/MediumQuestions/Q96UniqueBinarySearchTrees.java
+++ Tree/MediumQuestions/Q96UniqueBinarySearchTrees.java
@@ -5,7 +5,12 @@ Given an integer n, return the number of structurally unique BST's (binary searc
 has exactly n nodes of unique values from 1 to n.
 
 Example 1:
+Input: n = 3
+Output: 5
 
+Example 2:
+Input: n = 1
+Output: 1
 */
 
 public class Q96UniqueBinarySearchTrees {

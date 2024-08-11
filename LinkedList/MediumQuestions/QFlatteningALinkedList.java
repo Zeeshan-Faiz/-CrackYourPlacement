1d4e5872dc937361652ba38cac381bf38dc2aa6e
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-11 20:01:41 +0530

    Added QFlatteningALinkedList code

--- LinkedList/MediumQuestions/QFlatteningALinkedList.java
+++ LinkedList/MediumQuestions/QFlatteningALinkedList.java
@@ -1,5 +1,18 @@
 package LinkedList.MediumQuestions;
 
+/*
+Given a Linked List, where every node represents a sub-linked-list and contains two pointers:
+(i) a next pointer to the next node,
+(ii) a bottom pointer to a linked list where this node is head.
+Each of the sub-linked lists is in sorted order.
+Flatten the Link List so all the nodes appear in a single level while maintaining the sorted 
+order.
+Note: The flattened list will be printed using the bottom pointer instead of the next pointer. Look at the printList() function in the driver code for more clarity.
+
+Example 1:
+
+*/
+
 public class QFlatteningALinkedList {
     
 }

869c95ada41a19b08d8c4fc189837761408532d2
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-11 22:32:00 +0530

    Added QSortLinkedListOf0s1s2s code

--- Tree/MediumQuestions/Q96UniqueBinarySearchTrees.java
+++ Tree/MediumQuestions/Q96UniqueBinarySearchTrees.java
@@ -16,14 +16,14 @@ Output: 1
 public class Q96UniqueBinarySearchTrees {
 
     public int numTrees(int n) {
-        int[] dp = new int[n + 1];
-        dp[0] = 1;
-        dp[1] = 1;
+        
+        int[] catalan = new int[n + 1];
+        catalan[0] = catalan[1] = 1;
         for (int i = 2; i <= n; i++) {
             for (int j = 0; j < i; j++) {
-                dp[i] += dp[j] * dp[i - 1 - j];
+                catalan[i] += catalan[j] * catalan[i - 1 - j];
             }
         }
-        return dp[n];
+        return catalan[n];
     }
 }

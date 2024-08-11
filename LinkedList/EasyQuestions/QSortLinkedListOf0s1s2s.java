dada1b8279ee6bcbf82742ed46d0e229616fbf83
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-11 21:54:49 +0530

    Added QSortLinkedListOf0s1s2s code

--- LinkedList/EasyQuestions/QSortLinkedListOf0s1s2s.java
+++ LinkedList/EasyQuestions/QSortLinkedListOf0s1s2s.java
@@ -16,41 +16,32 @@ Output: 0 -> 1 -> 1 -> 1 -> 2 -> NULL
 public class QSortLinkedListOf0s1s2s {
     
     static Node sortList(Node head) {
+        
         if (head == null || head.next == null) 
             return head; 
 
-        // Create three dummy nodes to point to beginning of 
-        // three linked lists. These dummy nodes are created to 
-        // avoid null checks. 
-        Node zeroD = new Node(0); 
-        Node oneD = new Node(0); 
-        Node twoD = new Node(0);
+        // Create three dummy nodes to point to beginning of three linked lists.
+        Node zeroHead = new Node(0); 
+        Node oneHead = new Node(0); 
+        Node twoHead = new Node(0);
 
-        // Initialize current pointers for three 
-        // lists 
-        Node zero = zeroD, one = oneD, two = twoD; 
-
-        // Traverse list 
+        // Initialize current pointers for three lists 
+        Node zero = zeroHead, one = oneHead, two = twoHead; 
         Node curr = head; 
-        while (curr != null) { 
+        while (curr != null) 
+        { 
             if (curr.data == 0) { 
-                  
-                // If the data of current node is 0, 
-                // append it to pointer zero and update zero
+                // If the data of current node is 0, append it to pointer zero and update zero
                 zero.next = curr; 
                 zero = zero.next; 
             } 
-            else if (curr.data == 1) { 
-                  
-                // If the data of current node is 1, 
-                // append it to pointer one and update one
+            else if (curr.data == 1) {  
+                // If the data of current node is 1, append it to pointer one and update one
                 one.next = curr; 
                 one = one.next; 
             } 
             else { 
-                  
-                // If the data of current node is 2, 
-                // append it to pointer two and update two
+                // If the data of current node is 2, append it to pointer two and update two
                 two.next = curr; 
                 two = two.next; 
             } 
@@ -58,13 +49,12 @@ public class QSortLinkedListOf0s1s2s {
         } 
 
         // Combine the three lists
-        zero.next = (oneD.next != null) ? (oneD.next) : (twoD.next); 
-        one.next = twoD.next; 
+        zero.next = (oneHead.next != null) ? (oneHead.next) : (twoHead.next); 
+        one.next = twoHead.next; 
         two.next = null; 
           
         // Updated head 
-        head = zeroD.next; 
-
+        head = zeroHead.next; 
         return head; 
     } 
 }

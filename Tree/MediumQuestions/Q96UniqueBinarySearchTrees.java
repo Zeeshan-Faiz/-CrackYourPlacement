fee8addc7069d2aef51c3030e663d2bef36ef561
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-11 22:24:39 +0530

    Added Q96UniqueBinarySearchTrees code

--- Tree/MediumQuestions/Q96UniqueBinarySearchTrees.java
+++ Tree/MediumQuestions/Q96UniqueBinarySearchTrees.java
@@ -1,5 +1,13 @@
 package Tree.MediumQuestions;
 
+/*
+Given an integer n, return the number of structurally unique BST's (binary search trees) which 
+has exactly n nodes of unique values from 1 to n.
+
+Example 1:
+
+*/
+
 public class Q96UniqueBinarySearchTrees {
     
 }

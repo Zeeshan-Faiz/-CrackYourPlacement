ecbf87194cac22509f54b4769c7a18881b8ff61e
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-11 19:29:45 +0530

    Added QLargestSubArrayWith0Sum code

--- HeapPriorityQueue/MediumQuestions/QLargestSubArrayWith0Sum.java
+++ HeapPriorityQueue/MediumQuestions/QLargestSubArrayWith0Sum.java
@@ -5,7 +5,19 @@ Given an array having both positive and negative integers. The task is to comput
 length of the largest subarray with sum 0.
 
 Example 1:
+Input: arr[] = {15,-2,2,-8,1,7,10,23}, n = 8
+Output: 5
+Explanation: The largest subarray with sum 0 is -2 2 -8 1 7.
 
+Example 2:
+Input: arr[] = {2,10,4}, n = 3
+Output: 0
+Explanation: There is no subarray with 0 sum.
+
+Example 3:
+Input: arr[] = {1, 0, -4, 3, 1, 0}, n = 6
+Output: 5
+Explanation: The subarray is 0 -4 3 1 0.
 */
 
 public class QLargestSubArrayWith0Sum {

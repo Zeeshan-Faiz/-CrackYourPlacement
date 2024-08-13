6106318ffbcb5c7ea2a01890766a23b6d5fb4c31
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-13 20:34:13 +0530

    Added Q152MaximumProductSubArray code

--- DynamicProgramming/MediumQuestions/Q152MaximumProductSubArray.java
+++ DynamicProgramming/MediumQuestions/Q152MaximumProductSubArray.java
@@ -18,29 +18,29 @@ Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
 public class Q152MaximumProductSubArray {
 
     public int maxProduct(int[] nums) {
+
         if (nums == null || nums.length == 0)
             return 0;
 
         int n = nums.length;
-        long leftProduct = 1;
-        long rightProduct = 1;
-        long ans = nums[0];
+        long leftProd = 1;
+        long rightProd = 1;
+        long maxProd = nums[0];
 
         for (int i = 0; i < n; i++) {
 
-            // if any of leftProduct or rightProduct become 0 then update it to 1
-            leftProduct = (leftProduct == 0 || leftProduct < Integer.MIN_VALUE) ? 1 : leftProduct;
-            rightProduct = (rightProduct == 0 || rightProduct < Integer.MIN_VALUE) ? 1 : rightProduct;
+            // if any of leftProd or rightProd become 0 then update it to 1
+            leftProd = (leftProd == 0 || leftProd < Integer.MIN_VALUE) ? 1 : leftProd;
+            rightProd = (rightProd == 0 || rightProd < Integer.MIN_VALUE) ? 1 : rightProd;
 
             // prefix product
-            leftProduct *= nums[i];
+            leftProd = leftProd * nums[i];
 
             // suffix product
-            rightProduct *= nums[n - 1 - i];
+            rightProd = rightProd * nums[n - 1 - i];
 
-            ans = Math.max(ans, Math.max(leftProduct, rightProduct));
+            maxProd = Math.max(maxProd, Math.max(leftProd, rightProd));
         }
-
-        return (int) ans;
+        return (int) maxProd;
     }
 }

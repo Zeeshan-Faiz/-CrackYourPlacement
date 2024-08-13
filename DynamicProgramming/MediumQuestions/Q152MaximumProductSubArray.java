3f9655c4d9c28b7b9276e6b7593b2d4129803c77
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-13 20:33:04 +0530

    Added Q152MaximumProductSubArray code

--- DynamicProgramming/MediumQuestions/Q152MaximumProductSubArray.java
+++ DynamicProgramming/MediumQuestions/Q152MaximumProductSubArray.java
@@ -17,4 +17,30 @@ Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
 
 public class Q152MaximumProductSubArray {
 
+    public int maxProduct(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return 0;
+
+        int n = nums.length;
+        long leftProduct = 1;
+        long rightProduct = 1;
+        long ans = nums[0];
+
+        for (int i = 0; i < n; i++) {
+
+            // if any of leftProduct or rightProduct become 0 then update it to 1
+            leftProduct = (leftProduct == 0 || leftProduct < Integer.MIN_VALUE) ? 1 : leftProduct;
+            rightProduct = (rightProduct == 0 || rightProduct < Integer.MIN_VALUE) ? 1 : rightProduct;
+
+            // prefix product
+            leftProduct *= nums[i];
+
+            // suffix product
+            rightProduct *= nums[n - 1 - i];
+
+            ans = Math.max(ans, Math.max(leftProduct, rightProduct));
+        }
+
+        return (int) ans;
+    }
 }

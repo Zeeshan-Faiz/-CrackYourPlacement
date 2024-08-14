327606849f45eee295013b542ef069e8387807e9
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-14 22:24:43 +0530

    Added Q322CoinChange code

--- DynamicProgramming/MediumQuestions/Q322CoinChange.java
+++ DynamicProgramming/MediumQuestions/Q322CoinChange.java
@@ -24,14 +24,13 @@ Output: 0
 public class Q322CoinChange {
     
     public int coinChange(int[] coins, int amount) {
+        
         if (amount < 1)
             return 0;
 
-        // Create DP array
         int[] minCoinsDP = new int[amount + 1];
-
-        for (int i = 1; i <= amount; i++) {
-
+        for (int i = 1; i <= amount; i++) 
+        {
             minCoinsDP[i] = Integer.MAX_VALUE;
 
             // Try each coin
@@ -40,7 +39,6 @@ public class Q322CoinChange {
                     minCoinsDP[i] = Math.min(minCoinsDP[i], 1 + minCoinsDP[i - coin]);
             }
         }
-
         return minCoinsDP[amount] == Integer.MAX_VALUE ? -1 : minCoinsDP[amount];
     }
 }

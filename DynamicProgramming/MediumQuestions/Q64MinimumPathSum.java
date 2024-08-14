fff161814ff2f4873575fec26354ae2403c61b18
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-14 20:56:42 +0530

    Added Q64MinimumPathSum code

--- DynamicProgramming/MediumQuestions/Q64MinimumPathSum.java
+++ DynamicProgramming/MediumQuestions/Q64MinimumPathSum.java
@@ -1,5 +1,7 @@
 package DynamicProgramming.MediumQuestions;
 
+import java.util.Arrays;
+
 /*
 Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right, 
 which minimizes the sum of all numbers along its path.
@@ -16,5 +18,34 @@ Output: 12
 */
 
 public class Q64MinimumPathSum {
-    
+
+    public int minPathSum(int[][] grid) {
+
+        int m = grid.length, n = grid[0].length;
+        int dp[][] = new int[m][n];
+
+        // Initialize the DP matrix with -1
+        for (int row[] : dp)
+            Arrays.fill(row, -1);
+
+        // Call the helper function starting from the bottom-right cell
+        return helper(m - 1, n - 1, grid, dp);
+    }
+
+    private int helper(int i, int j, int[][] matrix, int[][] dp) {
+
+        if (i == 0 && j == 0)
+            return matrix[0][0]; // If we're at the top-left cell, return its value
+        if (i < 0 || j < 0)
+            return (int) Math.pow(10, 9); // If we're out of bounds, return a large value
+        if (dp[i][j] != -1)
+            return dp[i][j]; // If we've already calculated this cell, return the stored result
+
+        // Calculate the sum of the current cell plus the minimum sum path from above and from the left
+        int up = matrix[i][j] + helper(i - 1, j, matrix, dp);
+        int left = matrix[i][j] + helper(i, j - 1, matrix, dp);
+
+        // Store the minimum of the two possible paths
+        return dp[i][j] = Math.min(up, left);
+    }
 }

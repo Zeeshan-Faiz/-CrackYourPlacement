c5cdff13eb65a5c396ca8e63399bd8b0e0988e20
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-15 15:37:12 +0530

    Added QLongestCommonSubstring code

--- DynamicProgramming/MediumQuestions/QLongestCommonSubstring.java
+++ DynamicProgramming/MediumQuestions/QLongestCommonSubstring.java
@@ -15,4 +15,28 @@ Explanation: The longest common substrings are "A", "B", "C" all having length 1
 
 public class QLongestCommonSubstring {
     
+    public int longestCommonSubstr(String nums1, String nums2) {
+        
+        
+        char[] str1 = nums1.toCharArray();
+        char[] str2 = nums2.toCharArray();
+        int[][] dp = new int[str1.length + 1][str2.length + 1];
+
+        int max = 0;
+    
+        // Iterate over each position in the matrix
+        for (int i = 1; i <= str1.length; i++)
+          for (int j = 1; j <= str2.length; j++)
+    
+            // If numbers are equal
+            if (str1[i - 1] == str2[j - 1]) {
+              // Get the number from diagonally opposite
+              // and add 1
+              dp[i][j] = dp[i - 1][j - 1] + 1;
+    
+              max = Math.max(dp[i][j], max);
+            }
+    
+        return max;
+    }
 }

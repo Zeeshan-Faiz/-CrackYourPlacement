3a5e1786389d2f3258a2fd5ce63443a8cbb5dffd
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-15 15:36:32 +0530

    Added QLongestCommonSubstring code

--- DynamicProgramming/MediumQuestions/QLongestCommonSubstring.java
+++ DynamicProgramming/MediumQuestions/QLongestCommonSubstring.java
@@ -1,5 +1,13 @@
 package DynamicProgramming.MediumQuestions;
 
+/*
+You are given two strings m and n. Your task is to find the length of the longest common substring 
+among the given strings.
+
+Example 1:
+
+*/
+
 public class QLongestCommonSubstring {
     
 }

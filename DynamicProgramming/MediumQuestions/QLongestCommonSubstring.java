2c030bb3cfc715c13627a4834d5138f53f128439
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-15 15:36:59 +0530

    Added QLongestCommonSubstring code

--- DynamicProgramming/MediumQuestions/QLongestCommonSubstring.java
+++ DynamicProgramming/MediumQuestions/QLongestCommonSubstring.java
@@ -4,8 +4,13 @@ package DynamicProgramming.MediumQuestions;
 You are given two strings m and n. Your task is to find the length of the longest common substring 
 among the given strings.
 
-Example 1:
+Input: m = "ABCDGH", n = "ACDGHR"
+Output: 4
+Explanation: The longest common substring is "CDGH" which has length 4.
 
+Input: m = "ABC", n = "ACB"
+Output: 1
+Explanation: The longest common substrings are "A", "B", "C" all having length 1.
 */
 
 public class QLongestCommonSubstring {

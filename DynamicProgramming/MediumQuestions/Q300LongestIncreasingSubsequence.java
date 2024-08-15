6d1265349449f1652e0ff6bd93d54a51a0308108
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-15 16:30:26 +0530

    Added Q300LongestIncreasingSubsequence code

--- DynamicProgramming/MediumQuestions/Q300LongestIncreasingSubsequence.java
+++ DynamicProgramming/MediumQuestions/Q300LongestIncreasingSubsequence.java
@@ -19,4 +19,26 @@ Output: 1
 
 public class Q300LongestIncreasingSubsequence {
     
+    public int lengthOfLIS(int[] nums) {
+        int[] T = new int[nums.length];
+
+        // Start main pointer
+        for (int i = 1; i < nums.length; i++){
+
+            // Start second pointer
+            for (int j = 0; j < i; j++)
+                if (nums[i] > nums[j])
+                    if (T[j] + 1 > T[i])
+                        T[i] = T[j] + 1;
+            
+        }
+
+        // find the max value
+        int maxIndex = 0;
+        for (int i = 0; i < T.length; i++)
+            if (T[i] > T[maxIndex])
+                maxIndex = i;
+
+        return T[maxIndex] + 1;
+    }
 }

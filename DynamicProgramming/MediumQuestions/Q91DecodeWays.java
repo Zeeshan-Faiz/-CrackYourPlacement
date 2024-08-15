b949a12698f58493a1cfd27c038ad92e177f2f00
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-15 19:20:22 +0530

    Added Q91DecodeWays code

--- DynamicProgramming/MediumQuestions/Q91DecodeWays.java
+++ DynamicProgramming/MediumQuestions/Q91DecodeWays.java
@@ -43,28 +43,27 @@ Explanation:
 public class Q91DecodeWays {
     
     public int numDecodings(String s) {
+        
         if (s == null || s.length() == 0 || s.charAt(0) == '0') {
             return 0;
         }
 
         int n = s.length();
         int[] dp = new int[n + 1];
-        dp[0] = 1;
+        dp[0] = 1; // length of string 0 can be decoded only in 1 way
         dp[1] = 1;
 
-        for (int i = 2; i <= n; ++i) {
+        for (int i = 2; i <= n; ++i) 
+        {
             int oneDigit = s.charAt(i - 1) - '0';
             int twoDigits = Integer.parseInt(s.substring(i - 2, i));
-
-            if (oneDigit != 0) {
+            
+            if (oneDigit != 0)
                 dp[i] += dp[i - 1];
-            }
 
-            if (10 <= twoDigits && twoDigits <= 26) {
+            if (10 <= twoDigits && twoDigits <= 26)
                 dp[i] += dp[i - 2];
-            }
         }
-
         return dp[n];
     }
 }

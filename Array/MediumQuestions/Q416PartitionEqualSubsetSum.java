91cb6c6ebaa26639cc3c92ccedafd78bc0b4b8b2
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-16 18:27:06 +0530

    Added Q416PartitionEqualSubsetSum code

--- Array/MediumQuestions/Q416PartitionEqualSubsetSum.java
+++ Array/MediumQuestions/Q416PartitionEqualSubsetSum.java
@@ -18,7 +18,7 @@ Explanation: The array cannot be partitioned into equal sum subsets.
 public class Q416PartitionEqualSubsetSum {
 
     public boolean canPartition(int[] nums) {
-        
+
         int totalSum = 0;
         for (int n : nums)
             totalSum += n;
@@ -30,5 +30,23 @@ public class Q416PartitionEqualSubsetSum {
         Boolean[][] dp = new Boolean[nums.length][subSetSum + 1];
 
         return dfs(1, subSetSum, nums, dp);
+    }
+
+    public boolean dfs(int idx, int subSetSum, int[] nums, Boolean[][] dp) {
+        
+        if (subSetSum == 0)
+            return true;
+
+        if (idx == nums.length || subSetSum < 0)
+            return false;
+
+        if (dp[idx][subSetSum] != null)
+            return dp[idx][subSetSum];
+
+        //take current element or skip it
+        boolean result = dfs(idx + 1, subSetSum - nums[idx], nums, dp) ||
+                dfs(idx + 1, subSetSum, nums, dp);
+
+        return dp[idx][subSetSum] = result;
     }
 }

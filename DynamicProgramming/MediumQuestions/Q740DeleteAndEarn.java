c70e7ebacc7394c1bde5bbb731d37035f46ed881
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-16 17:02:47 +0530

    Added Q740DeleteAndEarn code

--- DynamicProgramming/MediumQuestions/Q740DeleteAndEarn.java
+++ DynamicProgramming/MediumQuestions/Q740DeleteAndEarn.java
@@ -1,5 +1,18 @@
 package DynamicProgramming.MediumQuestions;
 
+/*
+You are given an integer array nums. You want to maximize the number of points you get by 
+performing the following operation any number of times:
+    Pick any nums[i] and delete it to earn nums[i] points. Afterwards, you must delete every 
+    element equal to nums[i] - 1 and every element equal to nums[i] + 1.
+
+Return the maximum number of points you can earn by applying the above operation some number of 
+times.
+
+Example 1:
+
+*/
+
 public class Q740DeleteAndEarn {
     
 }

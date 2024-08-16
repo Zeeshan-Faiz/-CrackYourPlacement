572e8961ff915d577a2980da00c83e25098132a5
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-16 15:29:38 +0530

    Added Q221MaximalSquare code

--- DynamicProgramming/MediumQuestions/Q221MaximalSquare.java
+++ DynamicProgramming/MediumQuestions/Q221MaximalSquare.java
@@ -5,7 +5,16 @@ Given an m x n binary matrix filled with 0's and 1's, find the largest square co
 1's and return its area.
 
 Example 1:
+Input: matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+Output: 4
 
+Example 2:
+Input: matrix = [["0","1"],["1","0"]]
+Output: 1
+
+Example 3:
+Input: matrix = [["0"]]
+Output: 0
 */
 
 public class Q221MaximalSquare {

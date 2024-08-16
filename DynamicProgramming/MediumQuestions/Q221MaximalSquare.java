abc2408932dd2f638e3879ba346e0ff5c330198f
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-16 15:30:10 +0530

    Added Q221MaximalSquare code

--- DynamicProgramming/MediumQuestions/Q221MaximalSquare.java
+++ DynamicProgramming/MediumQuestions/Q221MaximalSquare.java
@@ -18,34 +18,33 @@ Output: 0
 */
 
 public class Q221MaximalSquare {
-    
+
     public int maximalSquare(char[][] matrix) {
+        
         int n = matrix.length;
         int m = matrix[0].length;
         int dp[][] = new int[n][m];
 
-        //fill first column and first row as is
+        // fill first column and first row as is
         for (int i = 0; i < m; i++)
             dp[0][i] = matrix[0][i] - '0';
         for (int i = 0; i < n; i++)
             dp[i][0] = matrix[i][0] - '0';
 
         for (int i = 1; i < n; i++) {
-            for (int j = 1; j < m; j++) 
-            {
+            for (int j = 1; j < m; j++) {
                 if (matrix[i][j] == '1')
                     dp[i][j] = 1 + Math.min(dp[i - 1][j], Math.min(dp[i][j - 1], dp[i - 1][j - 1]));
             }
         }
 
-        //find the max count for total square at that index, return it's area.
+        // find the max count for total square at that index, return it's area.
         int maxLength = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 maxLength = Math.max(maxLength, dp[i][j]);
             }
         }
-
         return maxLength * maxLength;
     }
 }

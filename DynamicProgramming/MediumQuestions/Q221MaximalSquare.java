9aaf70a7b1e54f615d50c54e75ac7e3a8d55f834
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-16 15:29:22 +0530

    Added Q221MaximalSquare code

--- DynamicProgramming/MediumQuestions/Q221MaximalSquare.java
+++ DynamicProgramming/MediumQuestions/Q221MaximalSquare.java
@@ -1,5 +1,13 @@
 package DynamicProgramming.MediumQuestions;
 
+/*
+Given an m x n binary matrix filled with 0's and 1's, find the largest square containing only 
+1's and return its area.
+
+Example 1:
+
+*/
+
 public class Q221MaximalSquare {
     
 }

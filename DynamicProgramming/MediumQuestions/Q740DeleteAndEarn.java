a2ee89c583da4d558b0a289056f13baff09dba47
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-16 17:03:17 +0530

    Added Q740DeleteAndEarn code

--- DynamicProgramming/MediumQuestions/Q740DeleteAndEarn.java
+++ DynamicProgramming/MediumQuestions/Q740DeleteAndEarn.java
@@ -29,4 +29,20 @@ You earn a total of 9 points.
 
 public class Q740DeleteAndEarn {
     
+    public int deleteAndEarn(int[] nums) {
+        int[] freq = new int[10002]; // max possible no a/q to constrain
+        int[] dp = new int[10002];
+
+        // find frequency of each element
+        for (int i = 0; i < nums.length; i++) {
+            freq[nums[i]]++;
+        }
+
+        dp[0] = 0;
+        dp[1] = 1 * freq[1];
+        for (int i = 2; i < 10002; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + i * freq[i]);
+        }
+        return dp[10001];
+    }
 }

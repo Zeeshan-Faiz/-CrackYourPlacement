3cb104e4f6d878426f61491e963d9460edd6dfab
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-17 16:08:23 +0530

    Added Q621TaskScheduler code

--- Greedy/MediumQuestions/Q621TaskScheduler.java
+++ Greedy/MediumQuestions/Q621TaskScheduler.java
@@ -1,5 +1,15 @@
 package Greedy.MediumQuestions;
 
+/*
+You are given an array of CPU tasks, each represented by letters A to Z, and a cooling time, n. 
+Each cycle or interval allows the completion of one task. Tasks can be completed in any order, but 
+there's a constraint: identical tasks must be separated by at least n intervals due to cooling time.
+​Return the minimum number of intervals required to complete all tasks.
+
+Example 1:
+
+*/
+
 public class Q621TaskScheduler {
     
 }

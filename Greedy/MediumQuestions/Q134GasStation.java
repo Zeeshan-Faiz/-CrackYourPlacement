f54e5b01ffb2b2b1c201b677507375fa462092dc
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-17 15:21:13 +0530

    Added Q134GasStation code

--- Greedy/MediumQuestions/Q134GasStation.java
+++ Greedy/MediumQuestions/Q134GasStation.java
@@ -36,10 +36,10 @@ Therefore, you can't travel around the circuit once no matter where you start.
 public class Q134GasStation {
 
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        
+
         if (arraySum(gas) < arraySum(cost))
             return -1;
-        int n = gas.length,answer = 0,sum = 0;
+        int n = gas.length, answer = 0, sum = 0;
         for (int i = 0; i < n; i++) {
             sum += (gas[i] - cost[i]);
             if (sum < 0) {
@@ -48,5 +48,14 @@ public class Q134GasStation {
             }
         }
         return answer;
+    }
+
+    private int arraySum(int[] arr) {
+        int n = arr.length;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+        }
+        return sum;
     }
 }

cb0cee3df1c0b2cdeb43ee90d502f3e484d90055
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-18 11:27:45 +0530

    Added Q435NonOverlappingIntervals code

--- Greedy/MediumQuestions/Q435NonOverlappingIntervals.java
+++ Greedy/MediumQuestions/Q435NonOverlappingIntervals.java
@@ -1,5 +1,7 @@
 package Greedy.MediumQuestions;
 
+import java.util.Arrays;
+
 /*
 Given an array of intervals intervals where intervals[i] = [starti, endi], return the minimum 
 number of intervals you need to remove to make the rest of the intervals non-overlapping.
@@ -22,4 +24,21 @@ Explanation: You don't need to remove any of the intervals since they're already
 
 public class Q435NonOverlappingIntervals {
     
+    public int eraseOverlapIntervals(int[][] intervals) {
+        int n = intervals.length;
+
+        //sort the interval wrt to end points
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
+
+        int prev = 0;
+        int count = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (intervals[i][0] >= intervals[prev][1]) {
+                prev = i;
+                count++;
+            }
+        }
+        return n - count;
+    }
 }

ea85ec664bc3466caaea2845bd712194c2d97a90
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-18 11:01:05 +0530

    Added Q402RemoveKdigits code

--- Greedy/MediumQuestions/Q402RemoveKdigits.java
+++ Greedy/MediumQuestions/Q402RemoveKdigits.java
@@ -1,5 +1,7 @@
 package Greedy.MediumQuestions;
 
+import java.util.Stack;
+
 /*
 Given string num representing a non-negative integer num, and an integer k, return the smallest 
 possible integer after removing k digits from num.
@@ -22,4 +24,36 @@ Explanation: Remove all the digits from the number and it is left with nothing w
 
 public class Q402RemoveKdigits {
     
+    public String removeKdigits(String num, int k) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char digit : num.toCharArray()) {
+            while (!stack.isEmpty() && k > 0 && stack.peek() > digit) {
+                stack.pop();
+                k--;
+            }
+            stack.push(digit);
+        }
+
+        // Remove remaining k digits from the end of the stack
+        while (k > 0 && !stack.isEmpty()) {
+            stack.pop();
+            k--;
+        }
+
+        // Construct the resulting string from the stack
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        sb.reverse(); // Reverse to get the correct order
+
+        // Remove leading zeros
+        while (sb.length() > 0 && sb.charAt(0) == '0') {
+            sb.deleteCharAt(0);
+        }
+
+        // Handle edge case where result might be empty
+        return sb.length() > 0 ? sb.toString() : "0";
+    }
 }

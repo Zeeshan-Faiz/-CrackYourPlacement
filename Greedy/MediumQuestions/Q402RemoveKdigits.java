2d5947ff9e1e75b667845ef3f1bf7ce4f0ca8c3f
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-18 11:00:35 +0530

    Added Q402RemoveKdigits code

--- Greedy/MediumQuestions/Q402RemoveKdigits.java
+++ Greedy/MediumQuestions/Q402RemoveKdigits.java
@@ -1,5 +1,13 @@
 package Greedy.MediumQuestions;
 
+/*
+Given string num representing a non-negative integer num, and an integer k, return the smallest 
+possible integer after removing k digits from num.
+
+Example 1:
+
+*/
+
 public class Q402RemoveKdigits {
     
 }

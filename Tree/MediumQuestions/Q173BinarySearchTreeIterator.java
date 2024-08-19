6fd1adaae8d96a681d219e43943dadbc14a5f9bf
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-19 10:13:39 +0530

    Added Q173BinarySearchTreeIterator code

--- Tree/MediumQuestions/Q173BinarySearchTreeIterator.java
+++ Tree/MediumQuestions/Q173BinarySearchTreeIterator.java
@@ -14,7 +14,23 @@ will return the smallest element in the BST.
 You may assume that next() calls will always be valid. That is, there will be at least a next number in the in-order traversal when next() is called.
 
 Example 1:
+Input
+["BSTIterator", "next", "next", "hasNext", "next", "hasNext", "next", "hasNext", "next", "hasNext"]
+[[[7, 3, 15, null, null, 9, 20]], [], [], [], [], [], [], [], [], []]
+Output
+[null, 3, 7, true, 9, true, 15, true, 20, false]
 
+Explanation
+BSTIterator bSTIterator = new BSTIterator([7, 3, 15, null, null, 9, 20]);
+bSTIterator.next();    // return 3
+bSTIterator.next();    // return 7
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 9
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 15
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 20
+bSTIterator.hasNext(); // return False
 */
 
 public class Q173BinarySearchTreeIterator {

8936a184e3cb95d782b2777e9c7b4aaa39da7749
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-19 22:24:59 +0530

    Added Q114FlattenBinaryTreeToLinkedList code

--- Tree/MediumQuestions/Q114FlattenBinaryTreeToLinkedList.java
+++ Tree/MediumQuestions/Q114FlattenBinaryTreeToLinkedList.java
@@ -17,4 +17,30 @@ Output: []
 
 public class Q114FlattenBinaryTreeToLinkedList {
     
+    public void flatten(TreeNode root) {
+        
+        TreeNode curr = root;
+        while (curr != null) 
+        {
+            // Check if the current node has a left child
+            if (curr.left != null) {
+                // If yes, find the rightmost node in the left subtree
+                TreeNode pre = curr.left;
+                while (pre.right != null) {
+                    pre = pre.right;
+                }
+
+                // Connect the rightmost node in the left subtree to the current node's right child
+                pre.right = curr.right;
+
+                // Move the entire left subtree to the right child of the current node
+                curr.right = curr.left;
+
+                // Set the left child of the current node to NULL
+                curr.left = null;
+            }
+            // Move to the next node on the right side
+            curr = curr.right;
+        }
+    }
 }

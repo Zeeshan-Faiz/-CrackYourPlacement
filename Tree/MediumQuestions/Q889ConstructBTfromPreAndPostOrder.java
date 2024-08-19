e1413eb2be25c681d0024e23867fe1095576ed83
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-19 10:38:04 +0530

    Added Q889ConstructBTfromPreAndPostOrder code

--- Tree/MediumQuestions/Q889ConstructBTfromPreAndPostOrder.java
+++ Tree/MediumQuestions/Q889ConstructBTfromPreAndPostOrder.java
@@ -6,7 +6,12 @@ binary tree of distinct values and postorder is the postorder traversal of the s
 reconstruct and return the binary tree. If there exist multiple answers, you can return any of them.
 
 Example 1:
+Input: preorder = [1,2,4,5,3,6,7], postorder = [4,5,2,6,7,3,1]
+Output: [1,2,3,4,5,6,7]
 
+Example 2:
+Input: preorder = [1], postorder = [1]
+Output: [1]
 */
 
 public class Q889ConstructBTfromPreAndPostOrder {

6683e9db9b9e30ffd7f04e5fb3409990cecbce3d
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-19 22:11:13 +0530

    Added Q99RecoverBinarySearchTree code

--- Tree/MediumQuestions/Q99RecoverBinarySearchTree.java
+++ Tree/MediumQuestions/Q99RecoverBinarySearchTree.java
@@ -1,5 +1,13 @@
 package Tree.MediumQuestions;
 
+/*
+You are given the root of a binary search tree (BST), where the values of exactly two nodes of 
+the tree were swapped by mistake. Recover the tree without changing its structure.
+
+Example 1:
+
+*/
+
 public class Q99RecoverBinarySearchTree {
     
 }

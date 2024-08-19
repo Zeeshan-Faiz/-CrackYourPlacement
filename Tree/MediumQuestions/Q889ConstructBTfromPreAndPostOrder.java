7323cd8fe64df16504965326334b2074ce4847f9
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-19 10:37:54 +0530

    Added Q889ConstructBTfromPreAndPostOrder code

--- Tree/MediumQuestions/Q889ConstructBTfromPreAndPostOrder.java
+++ Tree/MediumQuestions/Q889ConstructBTfromPreAndPostOrder.java
@@ -1,5 +1,14 @@
 package Tree.MediumQuestions;
 
+/*
+Given two integer arrays, preorder and postorder where preorder is the preorder traversal of a 
+binary tree of distinct values and postorder is the postorder traversal of the same tree, 
+reconstruct and return the binary tree. If there exist multiple answers, you can return any of them.
+
+Example 1:
+
+*/
+
 public class Q889ConstructBTfromPreAndPostOrder {
     
 }

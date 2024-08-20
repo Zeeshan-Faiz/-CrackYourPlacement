7175711b7a56af104dcd340f056fea681e1075ab
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-20 11:16:11 +0530

    Added Q662MaximumWidthOfBinaryTree code

--- Tree/MediumQuestions/Q662MaximumWidthOfBinaryTree.java
+++ Tree/MediumQuestions/Q662MaximumWidthOfBinaryTree.java
@@ -32,30 +32,29 @@ Explanation: The maximum width exists in the second level with length 2 (3,2).
 public class Q662MaximumWidthOfBinaryTree {
     
     public int widthOfBinaryTree(TreeNode root) {
-        if (root == null || (root.left == null && root.right == null)) {
+
+        if (root == null || (root.left == null && root.right == null))
             return 1;
-        }
 
         int maxWidth = 0;
-        // <Nodee, Weight>
         Map<TreeNode, Integer> map = new HashMap<>();
         map.put(root, 0);
-        Queue<TreeNode> qu = new LinkedList<>();
-        qu.offer(root);
-        while (!qu.isEmpty()) {
-            int size = qu.size();
-            int left = map.get(qu.peek());//
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            int left = map.get(queue.peek());//
             while (size-- > 0) {
-                TreeNode head = qu.poll();
+                TreeNode head = queue.poll();
                 maxWidth = Math.max(maxWidth, map.get(head) - left + 1);
                 if (head.left != null) {
-                    qu.offer(head.left);
+                    queue.offer(head.left);
                     map.put(head.left, 2 * map.get(head));
                 }
 
                 if (head.right != null) {
 
-                    qu.offer(head.right);
+                    queue.offer(head.right);
                     map.put(head.right, 2 * map.get(head) + 1);
                 }
             }

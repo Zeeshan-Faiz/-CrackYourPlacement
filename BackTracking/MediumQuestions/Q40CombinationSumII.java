9bb0b203df586df9b9e85a3f9a49cce1d19996a4
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-21 22:30:58 +0530

    Added Q40CombinationSumII code

--- BackTracking/MediumQuestions/Q40CombinationSumII.java
+++ BackTracking/MediumQuestions/Q40CombinationSumII.java
@@ -1,5 +1,9 @@
 package BackTracking.MediumQuestions;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /*
 Given a collection of candidate numbers (candidates) and a target number (target), find all unique 
 combinations in candidates where the candidate numbers sum to target. Each number in candidates 
@@ -18,4 +22,27 @@ Output:
 
 public class Q40CombinationSumII {
     
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(candidates); 
+        findCombinations(0, candidates, target, ans, new ArrayList<>()); 
+        return ans; 
+    }
+
+    private void findCombinations(int ind, int[] arr, int target, List<List<Integer>> ans, List<Integer> ds) {
+        if(target == 0) {
+            ans.add(new ArrayList<>(ds)); 
+            return; 
+        }
+        
+        for(int i = ind; i < arr.length;i++) {
+            if(i > ind && arr[i] == arr[i-1]) continue; 
+            if(arr[i]>target) break; 
+            
+            ds.add(arr[i]); 
+            findCombinations(i+1, arr, target - arr[i], ans, ds); 
+            ds.remove(ds.size() - 1); 
+        }
+    }
 }

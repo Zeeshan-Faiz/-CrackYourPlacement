6149a0ef2637b686999977a71a73c89c3a7c4428
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-21 10:51:08 +0530

    Added QCountBSTNodesInRange code

--- Tree/MediumQuestions/QCountBSTNodesInRange.java
+++ Tree/MediumQuestions/QCountBSTNodesInRange.java
@@ -42,4 +42,21 @@ public class QCountBSTNodesInRange {
 
         return count;
     }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+        TreeNode(int val) {
+            this.val = val;
+        }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }

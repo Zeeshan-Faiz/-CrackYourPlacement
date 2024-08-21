ed46edd0868a94954e772a16b6550e2b17aeb276
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-21 22:30:49 +0530

    Added Q40CombinationSumII code

--- BackTracking/MediumQuestions/Q40CombinationSumII.java
+++ BackTracking/MediumQuestions/Q40CombinationSumII.java
@@ -6,7 +6,14 @@ combinations in candidates where the candidate numbers sum to target. Each numbe
 may only be used once in the combination. Note: The solution set must not contain duplicate combinations.
 
 Example 1:
+Input: candidates = [10,1,2,7,6,1,5], target = 8
+Output: 
+[[1,1,6],[1,2,5],[1,7],[2,6]]
 
+Example 2:
+Input: candidates = [2,5,2,1,2], target = 5
+Output: 
+[[1,2,2],[5]]
 */
 
 public class Q40CombinationSumII {

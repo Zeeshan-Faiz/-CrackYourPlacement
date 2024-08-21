f35872bf1f4d4b60bdd6a5288105571da4b9a9b2
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-21 10:50:04 +0530

    Added QCountBSTNodesInRange code

--- Tree/MediumQuestions/QCountBSTNodesInRange.java
+++ Tree/MediumQuestions/QCountBSTNodesInRange.java
@@ -1,5 +1,15 @@
 package Tree.MediumQuestions;
 
+/*
+Given a Binary Search Tree (BST) and a range l-h(inclusive), count the number of nodes in the BST 
+that lie in the given range.
+    The values smaller than root go to the left side
+    The values greater and equal to the root go to the right side
+
+Example 1:
+
+*/
+
 public class QCountBSTNodesInRange {
     
 }

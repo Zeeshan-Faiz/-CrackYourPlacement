0a7d55320c8bab7d04650619a59004b259bf11a6
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-21 10:50:28 +0530

    Added QCountBSTNodesInRange code

--- Tree/MediumQuestions/QCountBSTNodesInRange.java
+++ Tree/MediumQuestions/QCountBSTNodesInRange.java
@@ -7,7 +7,24 @@ that lie in the given range.
     The values greater and equal to the root go to the right side
 
 Example 1:
+      10
+     /  \
+    5    50
+   /    /  \
+  1    40  100
+l = 5, h = 45
+Output: 3
+Explanation: 5 10 40 are the node in the range.
 
+Example 2:
+     5
+    /  \
+   4    6
+  /      \
+ 3        7
+l = 2, h = 8
+Output: 5
+Explanation: All the nodes are in the given range.
 */
 
 public class QCountBSTNodesInRange {

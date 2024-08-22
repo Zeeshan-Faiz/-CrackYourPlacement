6e9e92b04ba16d559ec9822abf444a41425249f1
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-22 10:04:58 +0530

    Added Q78Subsets code

--- BackTracking/MediumQuestions/Q78Subsets.java
+++ BackTracking/MediumQuestions/Q78Subsets.java
@@ -19,26 +19,23 @@ Output: [[],[0]]
 public class Q78Subsets {
 
     public List<List<Integer>> subsets(int[] nums) {
+        
         List<List<Integer>> resultList = new ArrayList<>();
-
-        // Start backtracking from the beginning
         backtrack(resultList, new ArrayList<>(), nums, 0);
         return resultList;
     }
 
-    private void backtrack(List<List<Integer>> resultSets, List<Integer> tempSet,
-            int[] nums, int start) {
-        // Add the set to result set
+    private void backtrack(List<List<Integer>> resultSets, List<Integer> tempSet, int[] nums, int start) {
+        
+        // Add the set to list
         resultSets.add(new ArrayList<>(tempSet));
-        for (int i = start; i < nums.length; i++) {
-
-            // Case of including the number
+        for (int i = start; i < nums.length; i++) 
+        {
+            // pick the element
             tempSet.add(nums[i]);
-
-            // Backtrack the new subset
             backtrack(resultSets, tempSet, nums, i + 1);
 
-            // Case of not-including the number
+            // backtrack and remove the picked element
             tempSet.remove(tempSet.size() - 1);
         }
     }

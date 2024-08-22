442a22e5139a9ad4db2eec1d0c74d76c08c9dd49
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-22 19:34:27 +0530

    Added Q131PalindromePartitioning code

--- BackTracking/MediumQuestions/Q131PalindromePartitioning.java
+++ BackTracking/MediumQuestions/Q131PalindromePartitioning.java
@@ -1,5 +1,13 @@
 package BackTracking.MediumQuestions;
 
+/*
+Given a string s, partition s such that every substring of the partition is a palindrome. 
+Return all possible palindrome partitioning of s.
+
+Example 1:
+
+*/
+
 public class Q131PalindromePartitioning {
     
 }

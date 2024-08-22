fc58e54498c32985be0158a38797ace94c37b681
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-22 19:34:52 +0530

    Added Q131PalindromePartitioning code

--- BackTracking/MediumQuestions/Q131PalindromePartitioning.java
+++ BackTracking/MediumQuestions/Q131PalindromePartitioning.java
@@ -1,5 +1,8 @@
 package BackTracking.MediumQuestions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
 Given a string s, partition s such that every substring of the partition is a palindrome. 
 Return all possible palindrome partitioning of s.
@@ -15,4 +18,32 @@ Output: [["a"]]
 
 public class Q131PalindromePartitioning {
     
+    public List<List<String>> partition(String s) {
+        List<List<String>> res= new ArrayList<>(); 
+        List<String> path = new ArrayList<>();
+        func(0, s, path, res);
+        return res;
+    }
+    
+    void func(int index, String s, List<String> path, List<List<String>> res) {
+        if(index == s.length()) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        for(int i = index; i < s.length(); ++i) {
+            if(isPalindrome(s, index, i)) {
+                path.add(s.substring(index, i+1));
+                func(i+1, s, path, res);
+                path.remove(path.size()-1);
+            }
+        }
+    }
+    
+    boolean isPalindrome(String s, int start, int end) {
+        while(start <= end) {
+            if(s.charAt(start++) != s.charAt(end--))
+                return false;
+        }
+        return true;
+    }
 }

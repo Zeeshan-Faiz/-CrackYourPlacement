6b3bc7fe17e4b07ac4d3d96b31b206e4434077b2
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-22 20:18:42 +0530

    Added Q46Permutations code

--- BackTracking/MediumQuestions/Q46Permutations.java
+++ BackTracking/MediumQuestions/Q46Permutations.java
@@ -1,5 +1,8 @@
 package BackTracking.MediumQuestions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
 Given an array nums of distinct integers, return all the possible permutations. You can return 
 the answer in any order.
@@ -19,4 +22,35 @@ Output: [[1]]
 
 public class Q46Permutations {
     
+    public List<List<Integer>> permute(int[] nums) {
+
+        List<List<Integer>> resultList = new ArrayList<>();
+        helper(resultList, new ArrayList<>(), nums);
+        return resultList;
+    }
+ 
+    private void helper(List<List<Integer>> resultList,
+                    ArrayList<Integer> tempList, int[] nums) {
+
+        // If we match the length, it is a permutation
+        if (tempList.size() == nums.length) {
+            resultList.add(new ArrayList<>(tempList));
+            return;
+        }
+
+        for (int number : nums) {
+
+             // Skip if we get same element
+            if (tempList.contains(number))
+                continue;
+            // Add the new element
+            tempList.add(number);
+
+            // Go back to try other element
+            helper(resultList, tempList, nums);
+
+            // Remove the element
+            tempList.remove(tempList.size() - 1);
+        }
+    }
 }

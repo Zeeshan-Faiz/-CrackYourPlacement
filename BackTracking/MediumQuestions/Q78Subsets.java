25e04f71fce3b56b3acfb1713bb1918e56e56484
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-22 10:05:48 +0530

    Added Q78Subsets code

--- BackTracking/MediumQuestions/Q78Subsets.java
+++ BackTracking/MediumQuestions/Q78Subsets.java
@@ -21,11 +21,11 @@ public class Q78Subsets {
     public List<List<Integer>> subsets(int[] nums) {
         
         List<List<Integer>> resultList = new ArrayList<>();
-        backtrack(resultList, new ArrayList<>(), nums, 0);
+        backtrack(0, nums,resultList, new ArrayList<>())
         return resultList;
     }
 
-    private void backtrack(List<List<Integer>> resultSets, List<Integer> tempSet, int[] nums, int start) {
+    private void backtrack(int ind, int[] nums, List<List<Integer>> resultSets, List<Integer> tempSet) {
         
         // Add the set to list
         resultSets.add(new ArrayList<>(tempSet));

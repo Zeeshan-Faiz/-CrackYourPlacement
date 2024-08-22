6a132d92392f1545270315c3825a2c47f2f1cec4
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-22 20:18:19 +0530

    Added Q46Permutations code

--- BackTracking/MediumQuestions/Q46Permutations.java
+++ BackTracking/MediumQuestions/Q46Permutations.java
@@ -1,5 +1,13 @@
 package BackTracking.MediumQuestions;
 
+/*
+Given an array nums of distinct integers, return all the possible permutations. You can return 
+the answer in any order.
+
+Example 1:
+
+*/
+
 public class Q46Permutations {
     
 }

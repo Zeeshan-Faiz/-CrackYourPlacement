c72387cb1272253d5a11e400895dd783b47067f1
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-22 10:19:23 +0530

    Added Q90SubsetsII code

--- BackTracking/MediumQuestions/Q90SubsetsII.java
+++ BackTracking/MediumQuestions/Q90SubsetsII.java
@@ -1,5 +1,9 @@
 package BackTracking.MediumQuestions;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /*
 Given an integer array nums that may contain duplicates, return all possible subsets (the power set).
 The solution set must not contain duplicate subsets. Return the solution in any order.
@@ -15,4 +19,20 @@ Output: [[],[0]]
 
 public class Q90SubsetsII {
     
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+         Arrays.sort(nums);
+        List<List<Integer>>ans=new ArrayList<>();
+        sub(0,nums,new ArrayList<>(),ans);
+        return ans;
+    }
+
+    public void sub(int ind,int[] nums,List<Integer>ds,List<List<Integer>>ans){
+        ans.add(new ArrayList<>(ds));
+        for(int i=ind;i<nums.length;i++){
+            if(i!=ind && nums[i]==nums[i-1]) continue;
+            ds.add(nums[i]);
+            sub(i+1,nums,ds,ans);
+            ds.remove(ds.size()-1);
+        }
+    }    
 }

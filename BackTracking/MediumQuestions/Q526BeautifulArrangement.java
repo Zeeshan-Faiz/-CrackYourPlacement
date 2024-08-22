bad6f5c2f10fce6d875c7574018f706741aeec8b
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-22 18:41:53 +0530

    Added Q526BeautifulArrangement code

--- BackTracking/MediumQuestions/Q526BeautifulArrangement.java
+++ BackTracking/MediumQuestions/Q526BeautifulArrangement.java
@@ -25,25 +25,27 @@ Output: 1
 */
 
 public class Q526BeautifulArrangement {
-    
-    private int count = 0;
 
+    private int count = 0;
     public int countArrangement(int n) {
-        if (n <= 0) {
+
+        if (n <= 0)
             return count;
-        }
+
         boolean[] visited = new boolean[n + 1];
         helper(visited, 1, n);
         return count;
     }
 
     private void helper(boolean[] visited, int valOrIndex, int n) {
+        
         if (valOrIndex > n) {
             count++;
             return;
         }
 
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n; i++) 
+        {
             if (!visited[i] && (valOrIndex % i == 0 || i % valOrIndex == 0)) {
                 visited[i] = true;
                 helper(visited, valOrIndex + 1, n);

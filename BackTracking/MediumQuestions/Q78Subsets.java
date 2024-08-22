3b7c04739bcb136471f5a24bf0c0cb9abe933212
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-22 10:04:01 +0530

    Added Q78Subsets code

--- BackTracking/MediumQuestions/Q78Subsets.java
+++ BackTracking/MediumQuestions/Q78Subsets.java
@@ -1,5 +1,8 @@
 package BackTracking.MediumQuestions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
 Given an integer array nums of unique elements, return all possible subsets (the power set).
 The solution set must not contain duplicate subsets. Return the solution in any order.
@@ -13,6 +16,30 @@ Input: nums = [0]
 Output: [[],[0]]
 */
 
-public class Q78Subsets{
+public class Q78Subsets {
 
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> resultList = new ArrayList<>();
+
+        // Start backtracking from the beginning
+        backtrack(resultList, new ArrayList<>(), nums, 0);
+        return resultList;
+    }
+
+    private void backtrack(List<List<Integer>> resultSets, List<Integer> tempSet,
+            int[] nums, int start) {
+        // Add the set to result set
+        resultSets.add(new ArrayList<>(tempSet));
+        for (int i = start; i < nums.length; i++) {
+
+            // Case of including the number
+            tempSet.add(nums[i]);
+
+            // Backtrack the new subset
+            backtrack(resultSets, tempSet, nums, i + 1);
+
+            // Case of not-including the number
+            tempSet.remove(tempSet.size() - 1);
+        }
+    }
 }

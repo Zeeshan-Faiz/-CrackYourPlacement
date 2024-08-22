7330dbcb897bb6513029821d9033f75eb222678e
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-22 08:50:57 +0530

    Added Q77Combinations code

--- BackTracking/MediumQuestions/Q77Combinations.java
+++ BackTracking/MediumQuestions/Q77Combinations.java
@@ -1,5 +1,13 @@
 package BackTracking.MediumQuestions;
 
+/*
+Given two integers n and k, return all possible combinations of k numbers chosen from the range 
+[1, n]. You may return the answer in any order.
+
+Example 1:
+
+*/
+
 public class Q77Combinations {
     
 }

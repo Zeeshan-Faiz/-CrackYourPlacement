b7fded7c7142326ce61a615b0672386dc4281bbf
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-22 19:36:20 +0530

    Added Q131PalindromePartitioning code

--- BackTracking/MediumQuestions/Q131PalindromePartitioning.java
+++ BackTracking/MediumQuestions/Q131PalindromePartitioning.java
@@ -20,21 +20,24 @@ public class Q131PalindromePartitioning {
 
     public List<List<String>> partition(String s) {
 
-        List<List<String>> res = new ArrayList<>();
-        func(0, s, res, new ArrayList<>());
-        return res;
+        List<List<String>> ans = new ArrayList<>();
+        helper(0, s, ans, new ArrayList<>());
+        return ans;
     }
 
-    void func(int index, String s, List<List<String>> res, List<String> path) {
+    void helper(int index, String s, List<List<String>> res, List<String> list) {
+        
         if (index == s.length()) {
-            res.add(new ArrayList<>(path));
+            res.add(new ArrayList<>(list));
             return;
         }
-        for (int i = index; i < s.length(); ++i) {
-            if (isPalindrome(s, index, i)) {
-                path.add(s.substring(index, i + 1));
-                func(i + 1, s, res, path);
-                path.remove(path.size() - 1);
+        for (int i = index; i < s.length(); i++) 
+        {
+            if (isPalindrome(s, index, i)) 
+            {
+                list.add(s.substring(index, i + 1));
+                helper(i + 1, s, res, list);
+                list.remove(list.size() - 1);
             }
         }
     }

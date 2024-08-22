344f94b521f190380ea907324120b4f464aebdb5
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-22 10:03:31 +0530

    Added Q78Subsets code

--- BackTracking/MediumQuestions/Q78Subsets.java
+++ BackTracking/MediumQuestions/Q78Subsets.java
@@ -1,5 +1,13 @@
 package BackTracking.MediumQuestions;
 
+/*
+Given an integer array nums of unique elements, return all possible subsets (the power set).
+The solution set must not contain duplicate subsets. Return the solution in any order.
+
+Example 1:
+
+*/
+
 public class Q78Subsets{
 
 }

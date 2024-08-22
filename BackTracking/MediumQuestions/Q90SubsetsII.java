f153e240f76bef098efbaa2197ccabd78b687bcb
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-22 10:24:30 +0530

    Added Q90SubsetsII code

--- BackTracking/MediumQuestions/Q90SubsetsII.java
+++ BackTracking/MediumQuestions/Q90SubsetsII.java
@@ -22,11 +22,11 @@ public class Q90SubsetsII {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> ans = new ArrayList<>();
-        sub(0, nums, ans, new ArrayList<>());
+        helper(0, nums, ans, new ArrayList<>());
         return ans;
     }
 
-    public void sub(int ind, int[] nums, List<List<Integer>> ans, List<Integer> list) {
+    public void helper(int ind, int[] nums, List<List<Integer>> ans, List<Integer> list) {
 
         ans.add(new ArrayList<>(list));
         for (int i = ind; i < nums.length; i++) 
@@ -35,7 +35,7 @@ public class Q90SubsetsII {
             if (i > ind && nums[i] == nums[i - 1])
                 continue;
             list.add(nums[i]);
-            sub(i + 1, nums, ans, list);
+            helper(i + 1, nums, ans, list);
             list.remove(list.size() - 1);// backtrack and remove the selected elements
         }
     }

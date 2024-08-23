0f11de3044153b8873074381581e105c5dcddece
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-23 20:05:18 +0530

    Added Q698PartitionToKEqualSubSetSum code

--- BackTracking/MediumQuestions/Q698PartitionToKEqualSubSetSum.java
+++ BackTracking/MediumQuestions/Q698PartitionToKEqualSubSetSum.java
@@ -15,44 +15,44 @@ Output: false
 */
 
 public class Q698PartitionToKEqualSubSetSum {
-    
+
     public boolean canPartitionKSubsets(int[] nums, int k) {
-        int total = 0;
+        
+        int totalSum = 0;
         for (int el : nums) {
-            total += el;
+            totalSum += el;
         }
 
-        if (total % k != 0) {
+        if (totalSum % k != 0 || nums.length < k) {
             return false;
         }
 
-        if (nums.length < k)
-            return false;
-
-        int subsetSum = total / k;
+        int subsetSum = totalSum / k;
         boolean[] visited = new boolean[nums.length];
-        return canPartition(nums, visited, 0, k, 0, subsetSum);
-
+        
+        return helper(nums, visited, 0, k, 0, subsetSum);
     }
 
-    private boolean canPartition(int[] nums, boolean[] visited, int start, int k, int curSum, int subsetSum) {
+    private boolean helper(int[] nums, boolean[] visited, int start, int k, int curSum, int subsetSum) {
+        
         if (k == 0)
             return true;
         if (curSum > subsetSum)
             return false;
-        if (curSum == subsetSum) {
-            return canPartition(nums, visited, 0, k - 1, 0, subsetSum);
-        }
 
-        for (int i = start; i < nums.length; i++) {
+        //if curSum equals subsetSum, we'll go for filling the next bucket
+        if (curSum == subsetSum)
+            return helper(nums, visited, 0, k - 1, 0, subsetSum);
+    
+        for (int i = start; i < nums.length; i++) 
+        {
             if (visited[i])
                 continue;
             visited[i] = true;
-            if (canPartition(nums, visited, i + 1, k, curSum + nums[i], subsetSum))
+            if (helper(nums, visited, i + 1, k, curSum + nums[i], subsetSum))
                 return true;
-            visited[i] = false;
+            visited[i] = false;//backtrack and unmark the element
         }
-
         return false;
     }
 }

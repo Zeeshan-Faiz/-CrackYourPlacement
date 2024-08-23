0bb8cf4ac0143d54f627eba777134b0d15278a03
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-23 19:31:12 +0530

    Added Q980UniquePathsIII code

--- BackTracking/HardQuestions/Q980UniquePathsIII.java
+++ BackTracking/HardQuestions/Q980UniquePathsIII.java
@@ -12,7 +12,20 @@ Return the number of 4-directional walks from the starting square to the ending 
 over every non-obstacle square exactly once.
 
 Example 1:
+Input: grid = [[1,0,0,0],[0,0,0,0],[0,0,2,-1]]
+Output: 2
+Explanation: We have the following two paths: 
+1. (0,0),(0,1),(0,2),(0,3),(1,3),(1,2),(1,1),(1,0),(2,0),(2,1),(2,2)
+2. (0,0),(1,0),(2,0),(2,1),(1,1),(0,1),(0,2),(0,3),(1,3),(1,2),(2,2)
 
+Example 2:
+Input: grid = [[1,0,0,0],[0,0,0,0],[0,0,0,2]]
+Output: 4
+Explanation: We have the following four paths: 
+1. (0,0),(0,1),(0,2),(0,3),(1,3),(1,2),(1,1),(1,0),(2,0),(2,1),(2,2),(2,3)
+2. (0,0),(0,1),(1,1),(1,0),(2,0),(2,1),(2,2),(1,2),(0,2),(0,3),(1,3),(2,3)
+3. (0,0),(1,0),(2,0),(2,1),(2,2),(1,2),(1,1),(0,1),(0,2),(0,3),(1,3),(2,3)
+4. (0,0),(1,0),(2,0),(2,1),(1,1),(0,1),(0,2),(0,3),(1,3),(1,2),(2,2),(2,3)
 */
 
 public class Q980UniquePathsIII {

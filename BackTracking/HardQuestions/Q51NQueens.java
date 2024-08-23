ad42c73cd71be31905333c377815c039199511c8
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-23 17:49:37 +0530

    Added Q51NQueens code

--- BackTracking/HardQuestions/Q51NQueens.java
+++ BackTracking/HardQuestions/Q51NQueens.java
@@ -8,7 +8,13 @@ Each solution contains a distinct board configuration of the n-queens' placement
 '.' both indicate a queen and an empty space, respectively.
 
 Example 1:
+Input: n = 4
+Output: [[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
+Explanation: There exist two distinct solutions to the 4-queens puzzle as shown above
 
+Example 2:
+Input: n = 1
+Output: [["Q"]]
 */
 
 public class Q51NQueens {

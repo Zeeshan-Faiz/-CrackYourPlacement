c817eabf9c3d74a449a671ba9ce758b968a6acb7
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-24 16:00:38 +0530

    Added Q733FloodFill code

--- Graph/EasyQuestions/Q733FloodFill.java
+++ Graph/EasyQuestions/Q733FloodFill.java
@@ -10,7 +10,15 @@ and so on. Replace the color of all of the aforementioned pixels with color.
 Return the modified image after performing the flood fill.
 
 Example 1:
+Input: image = [[1,1,1],[1,1,0],[1,0,1]], sr = 1, sc = 1, color = 2
+Output: [[2,2,2],[2,2,0],[2,0,1]]
+Explanation: From the center of the image with position (sr, sc) = (1, 1) (i.e., the red pixel), all pixels connected by a path of the same color as the starting pixel (i.e., the blue pixels) are colored with the new color.
+Note the bottom corner is not colored 2, because it is not 4-directionally connected to the starting pixel.
 
+Example 2:
+Input: image = [[0,0,0],[0,0,0]], sr = 0, sc = 0, color = 0
+Output: [[0,0,0],[0,0,0]]
+Explanation: The starting pixel is already colored 0, so no changes are made to the image.
 */
 
 public class Q733FloodFill {

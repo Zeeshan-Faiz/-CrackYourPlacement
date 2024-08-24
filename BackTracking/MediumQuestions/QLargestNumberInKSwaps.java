92a9df3f6d7f95a2a5bad12ea93c7df984704877
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-24 12:51:52 +0530

    Added QLargestNumberInKSwaps code

--- BackTracking/MediumQuestions/QLargestNumberInKSwaps.java
+++ BackTracking/MediumQuestions/QLargestNumberInKSwaps.java
@@ -1,5 +1,21 @@
 package BackTracking.MediumQuestions;
 
+/*
+Given a number K and string str of digits denoting a positive integer, build the largest number 
+possible by performing swap operations on the digits of str at most K times.
+
+Example 1:
+Input:
+K = 4
+str = "1234567"
+Output:
+7654321
+Explanation:
+Three swaps can make the
+input 1234567 to 7654321, swapping 1
+with 7, 2 with 6 and finally 3 with 5
+*/
+
 public class QLargestNumberInKSwaps{
     
 }

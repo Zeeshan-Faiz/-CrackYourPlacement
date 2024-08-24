f8a92fcba7847f297bda1323ef23cde614111a03
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-24 14:23:23 +0530

    Added QBFSOfGraph code

--- Graph/EasyQuestions/QBFSOfGraph.java
+++ Graph/EasyQuestions/QBFSOfGraph.java
@@ -7,6 +7,20 @@ of the graph starting from the 0th vertex, from left to right according to the i
 you should only take nodes directly or indirectly connected from Node 0 in consideration.
 
 Example 1:
+Input:
+V = 5, E = 4
+adj = {{1,2,3},{},{4},{},{}}
+
+Output: 
+0 1 2 3 4
+Explanation: 
+0 is connected to 1 , 2 , 3.
+2 is connected to 4.
+so starting from 0, it will go to 1 then 2
+then 3. After this 2 to 4, thus bfs will be
+0 1 2 3 4.
+
+Example 2:
 
 */
 

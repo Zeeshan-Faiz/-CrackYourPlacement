622973a5d71ffc206d4cf5984258e38ac26c29af
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-24 14:50:42 +0530

    Added QDFSOFGraph code

--- Graph/EasyQuestions/QDFSOFGraph.java
+++ Graph/EasyQuestions/QDFSOFGraph.java
@@ -1,5 +1,14 @@
 package Graph.EasyQuestions;
 
+/*
+You are given a connected undirected graph. Perform a Depth First Traversal of the graph.
+Note: Use the recursive approach to find the DFS traversal of the graph starting from the 0th 
+vertex from left to right according to the graph.
+
+Example 1:
+
+*/
+
 public class QDFSOFGraph {
     
 }

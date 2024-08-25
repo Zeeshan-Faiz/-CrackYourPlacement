dce11c0a25fc269b006fecb89a5facd34377b3e4
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-25 18:19:31 +0530

    Added QDirectedGraphCycle code

--- Graph/MediumQuestions/QDirectedGraphCycle.java
+++ Graph/MediumQuestions/QDirectedGraphCycle.java
@@ -1,5 +1,13 @@
 package Graph.MediumQuestions;
 
+/*
+Given a Directed Graph with V vertices (Numbered from 0 to V-1) and E edges, check whether it 
+contains any cycle or not.
+
+Example 1:
+
+*/
+
 public class QDirectedGraphCycle {
     
 }

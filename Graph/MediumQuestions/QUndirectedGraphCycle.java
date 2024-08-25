a2017fe955d4cc6d4ccae1792ec95e0a6d090c7f
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-25 17:51:29 +0530

    Added QUndirectedGraphCycle code

--- Graph/MediumQuestions/QUndirectedGraphCycle.java
+++ Graph/MediumQuestions/QUndirectedGraphCycle.java
@@ -6,6 +6,14 @@ contains any cycle or not. Graph is in the form of adjacency list where adj[i] c
 nodes ith node is having edge with.
 
 Example 1:
+Input:  
+V = 5, E = 5
+adj = {{1}, {0, 2, 4}, {1, 3}, {2, 4}, {1, 3}} 
+Output: 1
+Explanation: 
+1->2->3->4->1 is a cycle.
+
+Example 2:
 
 */
 

f1b3d86201c914b31c74510316de88e245edaeb2
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-25 18:20:02 +0530

    Added QDirectedGraphCycle code

--- Graph/MediumQuestions/QDirectedGraphCycle.java
+++ Graph/MediumQuestions/QDirectedGraphCycle.java
@@ -8,39 +8,40 @@ contains any cycle or not.
 */
 
 public class QDirectedGraphCycle {
-    
+
     public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
         // code here
         int vis[] = new int[V];
         int pathVis[] = new int[V];
-        
-        for(int i = 0;i<V;i++) {
-            if(vis[i] == 0) {
-                if(dfsCheck(i, adj, vis, pathVis) == true) return true; 
+
+        for (int i = 0; i < V; i++) {
+            if (vis[i] == 0) {
+                if (dfsCheck(i, adj, vis, pathVis) == true)
+                    return true;
             }
         }
-        return false; 
+        return false;
     }
-    
+
     private boolean dfsCheck(int node, ArrayList<ArrayList<Integer>> adj, int vis[], int pathVis[]) {
-        vis[node] = 1; 
-        pathVis[node] = 1; 
-        
-        // traverse for adjacent nodes 
-        for(int it : adj.get(node)) {
-            // when the node is not visited 
-            if(vis[it] == 0) {
-                if(dfsCheck(it, adj, vis, pathVis) == true) 
-                    return true; 
+        vis[node] = 1;
+        pathVis[node] = 1;
+
+        // traverse for adjacent nodes
+        for (int it : adj.get(node)) {
+            // when the node is not visited
+            if (vis[it] == 0) {
+                if (dfsCheck(it, adj, vis, pathVis) == true)
+                    return true;
             }
             // if the node has been previously visited
-            // but it has to be visited on the same path 
-            else if(pathVis[it] == 1) {
-                return true; 
+            // but it has to be visited on the same path
+            else if (pathVis[it] == 1) {
+                return true;
             }
         }
-        
-        pathVis[node] = 0; 
-        return false; 
+
+        pathVis[node] = 0;
+        return false;
     }
 }

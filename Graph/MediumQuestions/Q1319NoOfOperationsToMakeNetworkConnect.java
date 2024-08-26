59e156b2a4c130f5cbc9f9311ec32035444607bc
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-26 20:32:05 +0530

    Added Q1319NoOfOperationsToMakeNetworkConnect code

--- Graph/MediumQuestions/Q1319NoOfOperationsToMakeNetworkConnect.java
+++ Graph/MediumQuestions/Q1319NoOfOperationsToMakeNetworkConnect.java
@@ -1,5 +1,8 @@
 package Graph.MediumQuestions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
 There are n computers numbered from 0 to n - 1 connected by ethernet cables connections forming a 
 network where connections[i] = [ai, bi] represents a connection between computers ai and bi. Any 
@@ -28,7 +31,7 @@ Explanation: There are not enough cables.
 public class Q1319NoOfOperationsToMakeNetworkConnect {
 
     public int makeConnected(int n, int[][] edge) {
-        
+
         DisjointSet ds = new DisjointSet(n);
         int cntExtras = 0;
         int m = edge.length;
@@ -50,5 +53,59 @@ public class Q1319NoOfOperationsToMakeNetworkConnect {
         if (cntExtras >= ans)
             return ans;
         return -1;
+    }
+
+    class DisjointSet {
+        
+        List<Integer> rank = new ArrayList<>();
+        List<Integer> parent = new ArrayList<>();
+        List<Integer> size = new ArrayList<>();
+
+        public DisjointSet(int n) {
+            for (int i = 0; i <= n; i++) {
+                rank.add(0);
+                parent.add(i);
+                size.add(1);
+            }
+        }
+
+        public int findUPar(int node) {
+            if (node == parent.get(node)) {
+                return node;
+            }
+            int ulp = findUPar(parent.get(node));
+            parent.set(node, ulp);
+            return parent.get(node);
+        }
+
+        public void unionByRank(int u, int v) {
+            int ulp_u = findUPar(u);
+            int ulp_v = findUPar(v);
+            if (ulp_u == ulp_v)
+                return;
+            if (rank.get(ulp_u) < rank.get(ulp_v)) {
+                parent.set(ulp_u, ulp_v);
+            } else if (rank.get(ulp_v) < rank.get(ulp_u)) {
+                parent.set(ulp_v, ulp_u);
+            } else {
+                parent.set(ulp_v, ulp_u);
+                int rankU = rank.get(ulp_u);
+                rank.set(ulp_u, rankU + 1);
+            }
+        }
+
+        public void unionBySize(int u, int v) {
+            int ulp_u = findUPar(u);
+            int ulp_v = findUPar(v);
+            if (ulp_u == ulp_v)
+                return;
+            if (size.get(ulp_u) < size.get(ulp_v)) {
+                parent.set(ulp_u, ulp_v);
+                size.set(ulp_v, size.get(ulp_v) + size.get(ulp_u));
+            } else {
+                parent.set(ulp_v, ulp_u);
+                size.set(ulp_u, size.get(ulp_u) + size.get(ulp_v));
+            }
+        }
     }
 }

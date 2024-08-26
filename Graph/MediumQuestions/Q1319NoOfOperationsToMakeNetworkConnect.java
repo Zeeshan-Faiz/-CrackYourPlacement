633fc4e1719d1acfb8a7a41e64f0c46bbc9032db
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-26 20:31:48 +0530

    Added Q1319NoOfOperationsToMakeNetworkConnect code

--- Graph/MediumQuestions/Q1319NoOfOperationsToMakeNetworkConnect.java
+++ Graph/MediumQuestions/Q1319NoOfOperationsToMakeNetworkConnect.java
@@ -26,5 +26,29 @@ Explanation: There are not enough cables.
 */
 
 public class Q1319NoOfOperationsToMakeNetworkConnect {
-    
+
+    public int makeConnected(int n, int[][] edge) {
+        
+        DisjointSet ds = new DisjointSet(n);
+        int cntExtras = 0;
+        int m = edge.length;
+        for (int i = 0; i < m; i++) {
+            int u = edge[i][0];
+            int v = edge[i][1];
+            if (ds.findUPar(u) == ds.findUPar(v)) {
+                cntExtras++;
+            } else {
+                ds.unionBySize(u, v);
+            }
+        }
+        int cntC = 0;
+        for (int i = 0; i < n; i++) {
+            if (ds.parent.get(i) == i)
+                cntC++;
+        }
+        int ans = cntC - 1;
+        if (cntExtras >= ans)
+            return ans;
+        return -1;
+    }
 }

ac0e9aeb075076caabea24fd0d9910a5f6fadbf5
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-26 14:01:47 +0530

    Added Q394DecodeString code

--- String/MediumQuestions/Q394DecodeString.java
+++ String/MediumQuestions/Q394DecodeString.java
@@ -1,5 +1,7 @@
 package String.MediumQuestions;
 
+import java.util.Stack;
+
 /*
 Given an encoded string, return its decoded string.
 
@@ -26,4 +28,51 @@ Output: "abcabccdcdcdef"
 
 public class Q394DecodeString {
     
+    public String decodeString(String s) {
+        Stack<Integer> numStack = new Stack<>();
+        Stack<String> stringStack = new Stack<>();
+        int k = 0;
+
+        for (char c : s.toCharArray()) {
+
+            if (Character.isDigit(c)) {
+                k = (k * 10) + (c - '0');
+                continue;
+            }
+
+            if (c == '[') {
+                numStack.push(k);
+                k = 0;
+                stringStack.push(String.valueOf(c));
+                continue;
+            }
+
+            if (c != ']') {
+                stringStack.push(String.valueOf(c));
+                continue;
+            }
+
+            StringBuilder temp = new StringBuilder();
+            while (!stringStack.peek().equals("["))
+                temp.insert(0, stringStack.pop());
+
+            // remove the "["
+            stringStack.pop();
+
+            // Get the new string
+            StringBuilder replacement = new StringBuilder();
+            int count = numStack.pop();
+            for (int i = 0; i < count; i++)
+                replacement.append(temp);
+
+            // Add it to the stack
+            stringStack.push(replacement.toString());
+        }
+
+        StringBuilder result = new StringBuilder();
+        while (!stringStack.empty()) {
+            result.insert(0, stringStack.pop());
+        }
+        return result.toString();
+    }
 }

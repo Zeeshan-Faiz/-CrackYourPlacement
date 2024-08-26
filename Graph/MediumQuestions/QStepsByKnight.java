3f6b496081877d942d82d72b9437df08e97a11fe
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-26 15:32:33 +0530

    Added QStepsByKnight code

--- Graph/MediumQuestions/QStepsByKnight.java
+++ Graph/MediumQuestions/QStepsByKnight.java
@@ -1,5 +1,16 @@
 package Graph.MediumQuestions;
 
+/*
+Given a square chessboard, the initial position of Knight and position of a target. Find out the 
+minimum steps a Knight will take to reach the target position.
+Note:
+The initial and the target position coordinates of Knight have been given according to 1-base 
+indexing.
+
+Example 1:
+
+*/
+
 public class QStepsByKnight {
     
 }

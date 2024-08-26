81df6c33b72e7bc938843a97fb144bd612ef248c
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-26 15:32:48 +0530

    Added QStepsByKnight code

--- Graph/MediumQuestions/QStepsByKnight.java
+++ Graph/MediumQuestions/QStepsByKnight.java
@@ -8,7 +8,18 @@ The initial and the target position coordinates of Knight have been given accord
 indexing.
 
 Example 1:
+Input:
+N=6
+knightPos[ ] = {4, 5}
+targetPos[ ] = {1, 1}
 
+Output:
+3
+
+Explanation:
+Knight takes 3 step to reach from 
+(4, 5) to (1, 1):
+(4, 5) -> (5, 3) -> (3, 2) -> (1, 1).
 */
 
 public class QStepsByKnight {

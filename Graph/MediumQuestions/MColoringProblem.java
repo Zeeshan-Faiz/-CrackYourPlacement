11a22b06e1be7b9e55ac5e78309cb91213d71963
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-27 21:02:45 +0530

    Added MColoringProblem code

--- Graph/MediumQuestions/MColoringProblem.java
+++ Graph/MediumQuestions/MColoringProblem.java
@@ -10,12 +10,28 @@ possible to colour vertices and 0 otherwise.
 */
 
 public class MColoringProblem {
-    
-    public static boolean graphColoring(List < Integer > [] G, int[] color, int i, int C) {
+
+    public static boolean graphColoring(List<Integer>[] G, int[] color, int i, int C) {
 
         int n = G.length;
-        if (solve(i, G, color, n, C) == true) 
+        if (solve(i, G, color, n, C) == true)
             return true;
+        return false;
+    }
+
+    private static boolean solve(int node, List<Integer>[] G, int[] color, int n, int m) {
+        
+        if (node == n)
+            return true;
+
+        for (int i = 1; i <= m; i++) {
+            if (isSafe(node, G, color, n, i)) {
+                color[node] = i;
+                if (solve(node + 1, G, color, n, m) == true)
+                    return true;
+                color[node] = 0;
+            }
+        }
         return false;
     }
 }

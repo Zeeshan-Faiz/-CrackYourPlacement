1c186ec69c5445fc6734f760caffc0f109d78645
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-27 21:02:04 +0530

    Added MColoringProblem code

--- Graph/MediumQuestions/MColoringProblem.java
+++ Graph/MediumQuestions/MColoringProblem.java
@@ -1,7 +1,10 @@
 package Graph.MediumQuestions;
 
 /*
-
+Given an undirected graph and an integer M. The task is to determine if the graph can be colored 
+with at most M colors such that no two adjacent vertices of the graph are colored with the same 
+color. Here coloring of a graph means the assignment of colors to all vertices. Print 1 if it is 
+possible to colour vertices and 0 otherwise.
 */
 
 public class MColoringProblem {

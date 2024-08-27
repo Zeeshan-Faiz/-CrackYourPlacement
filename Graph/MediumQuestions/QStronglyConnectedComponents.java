5f95f7b43736ce6556384a0c399a52163cd1f7bd
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-27 11:06:37 +0530

    Added QStronglyConnectedComponents code

--- Graph/MediumQuestions/QStronglyConnectedComponents.java
+++ Graph/MediumQuestions/QStronglyConnectedComponents.java
@@ -1,5 +1,18 @@
 package Graph.MediumQuestions;
 
+/*
+Given a Directed Graph with V vertices (Numbered from 0 to V-1) and E edges, Find the number of 
+strongly connected components in the graph.
+
+Input:
+0 --> 1 --> 2 --> 0
+Output:
+1
+Explanation:
+All of the nodes are connected to each other.
+So, there's only one SCC.
+*/
+
 public class QStronglyConnectedComponents {
     
 }

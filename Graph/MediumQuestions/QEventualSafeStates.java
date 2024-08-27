401e088f544d56dcc90efb540415b300649fa5d6
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-27 10:30:17 +0530

    Added QEventualSafeStates code

--- Graph/MediumQuestions/QEventualSafeStates.java
+++ Graph/MediumQuestions/QEventualSafeStates.java
@@ -9,7 +9,15 @@ You have to return an array containing all the safe nodes of the graph. The answ
 in ascending order.
 
 Example 1:
-
+Input:
+Output:
+2 4 5 6
+Explanation:
+The given graph is shown above.
+Nodes 5 and 6 are terminal nodes as there are no 
+outgoing edges from either of them. 
+Every path starting at nodes 2, 4, 5, and 6 all 
+lead to either node 5 or 6.
 */
 
 public class QEventualSafeStates {

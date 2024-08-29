716b2946bfcbc572c275228df06ecc6d87767f7a
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-29 20:36:23 +0530

    Added QTopologicalSortUsingBFS code

--- Graph/MediumQuestions/QTopologicalSortUsingBFS.java
+++ Graph/MediumQuestions/QTopologicalSortUsingBFS.java
@@ -1,7 +1,8 @@
 package Graph.MediumQuestions;
 
 import java.util.ArrayList;
-import java.util.Stack;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /*
 Given an adjacency list for a Directed Acyclic Graph (DAG) where adj_list[i] contains a list of 
@@ -30,6 +31,39 @@ public class QTopologicalSortUsingBFS {
 
     public int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) {
 
-        
+        int indegree[] = new int[V];
+        // find indegree for each node
+        for (int i = 0; i < V; i++) {
+            for (int it : adj.get(i)) {
+                indegree[it]++;
+            }
+        }
+
+        Queue<Integer> queue = new LinkedList<Integer>();
+        //add all nodes with indegree=0
+        for (int i = 0; i < V; i++) {
+            if (indegree[i] == 0) {
+                queue.add(i);
+            }
+        }
+
+        int topo[] = new int[V];
+        int i = 0;
+        while (!queue.isEmpty()) {
+            int node = queue.peek();
+            queue.remove();
+            topo[i++] = node;
+            // node is in your topo sort
+            // so please remove it from the indegree
+
+            for (int it : adj.get(node)) {
+                indegree[it]--;
+                if (indegree[it] == 0) {
+                    queue.add(it);
+                }
+            }
+        }
+
+        return topo;
     }
 }

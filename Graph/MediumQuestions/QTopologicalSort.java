6f59cca66ade505637e87d77929761f0793dd8e0
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-29 20:00:11 +0530

    Added QTopologicalSort code

--- Graph/MediumQuestions/QTopologicalSort.java
+++ Graph/MediumQuestions/QTopologicalSort.java
@@ -1,5 +1,8 @@
 package Graph.MediumQuestions;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 /*
 Given an adjacency list for a Directed Acyclic Graph (DAG) where adj_list[i] contains a list of 
 all vertices j such that there is a directed edge from vertex i to vertex j, with  V  vertices and 
@@ -25,4 +28,31 @@ would be 1 for all test cases. One possible Topological order for the graph is 5
 
 public class QTopologicalSort {
     
+    public int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) 
+    {
+        // add your code here
+        int vis[] = new int[V];
+        Stack<Integer> st = new Stack<Integer>();
+        for (int i = 0; i < V; i++) {
+            if (vis[i] == 0) {
+                dfs(i, vis, st, adj);
+            }
+        }
+
+        int ans[] = new int[V];
+        int i = 0;
+        while (!st.isEmpty()) {
+            ans[i++] = st.pop();
+        }
+        return ans;
+    }
+    private static void dfs(int node, int vis[], Stack<Integer> st,
+            ArrayList<ArrayList<Integer>> adj) {
+        vis[node] = 1;
+        for (int it : adj.get(node)) {
+            if (vis[it] == 0)
+                dfs(it, vis, st, adj);
+        }
+        st.push(node);
+    }
 }

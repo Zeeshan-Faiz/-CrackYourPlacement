eaa180af3d1b0f2c4c9f0582a46398df7191e373
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-29 18:28:18 +0530

    Added QDjikstraAlgorithm code

--- Graph/MediumQuestions/QDjikstraAlgorithm.java
+++ Graph/MediumQuestions/QDjikstraAlgorithm.java
@@ -51,29 +51,26 @@ public class QDjikstraAlgorithm {
         dist[S] = 0; // Source initialised with dist=0.
         pq.add(new Pair(0, S));
 
-        // Now, pop the minimum distance node first from the min-heap
-        // and traverse for all its adjacent nodes.
-        while (pq.size() != 0) {
+        // Now, pop the minimum distance node first from the min-heap and traverse for all its adjacent nodes.
+        while (pq.size() != 0) 
+        {
             int dis = pq.peek().distance;
             int node = pq.peek().node;
             pq.remove();
 
-            // Check for all adjacent nodes of the popped out
-            // element whether the prev dist is larger than current or not.
+            // Check for all adjacent nodes prev dist is larger than current or not.
             for (int i = 0; i < adj.get(node).size(); i++) {
                 int edgeWeight = adj.get(node).get(i).get(1);
                 int adjNode = adj.get(node).get(i).get(0);
 
-                // If current distance is smaller,
-                // push it into the queue.
+                // If current distance is smaller, push it into the queue.
                 if (dis + edgeWeight < dist[adjNode]) {
                     dist[adjNode] = dis + edgeWeight;
                     pq.add(new Pair(dist[adjNode], adjNode));
                 }
             }
         }
-        // Return the list containing shortest distances
-        // from source to all the nodes.
+        // Return the list containing shortest distances from source to all the nodes.
         return dist;
     }
 }

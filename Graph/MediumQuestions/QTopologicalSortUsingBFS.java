6012d5aaedb0da6853b10656e9967fa91097eb80
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-29 20:35:34 +0530

    Added QTopologicalSortUsingBFS code

--- Graph/MediumQuestions/QTopologicalSortUsingBFS.java
+++ Graph/MediumQuestions/QTopologicalSortUsingBFS.java
@@ -30,28 +30,6 @@ public class QTopologicalSortUsingBFS {
 
     public int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) {
 
-        int vis[] = new int[V];
-        Stack<Integer> stack = new Stack<Integer>();
-        for (int i = 0; i < V; i++) {
-            if (vis[i] == 0) {
-                dfs(i, vis, stack, adj);
-            }
-        }
-
-        int ans[] = new int[V];
-        int i = 0;
-        while (!stack.isEmpty()) {
-            ans[i++] = stack.pop();
-        }
-        return ans;
-    }
-
-    private static void dfs(int node, int vis[], Stack<Integer> stack, ArrayList<ArrayList<Integer>> adj) {
-        vis[node] = 1;
-        for (int it : adj.get(node)) {
-            if (vis[it] == 0)
-                dfs(it, vis, stack, adj);
-        }
-        stack.push(node);
+        
     }
 }

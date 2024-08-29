87925af4b6d8ffb7c0a8d706dfab883171294f32
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-29 19:59:54 +0530

    Added QTopologicalSort code

--- Graph/MediumQuestions/QTopologicalSort.java
+++ Graph/MediumQuestions/QTopologicalSort.java
@@ -7,7 +7,20 @@ E  edges, your task is to find any valid topological sorting of the graph.
 In a topological sort, for every directed edge u -> v,  u must come before v in the ordering.
 
 Example 1:
+Input:
+Output:
+1
+Explanation:
+The output 1 denotes that the order is valid. So, if you have, implemented your function correctly, then output
+would be 1 for all test cases. One possible Topological order for the graph is 3, 2, 1, 0.
 
+Example 2:
+Input:
+Output:
+1
+Explanation:
+The output 1 denotes that the order is valid. So, if you have, implemented your function correctly, then output
+would be 1 for all test cases. One possible Topological order for the graph is 5, 4, 2, 1, 3, 0.
 */
 
 public class QTopologicalSort {

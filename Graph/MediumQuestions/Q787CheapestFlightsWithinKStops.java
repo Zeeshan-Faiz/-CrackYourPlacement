4d932b34ff16649d3f7e04e2ff5eb64c1f6232a6
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-29 22:34:46 +0530

    Added Q787CheapestFlightsWithinKStops code

--- Graph/MediumQuestions/Q787CheapestFlightsWithinKStops.java
+++ Graph/MediumQuestions/Q787CheapestFlightsWithinKStops.java
@@ -1,5 +1,9 @@
 package Graph.MediumQuestions;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 /*
 There are n cities connected by some number of flights. You are given an array flights where 
 flights[i] = [fromi, toi, pricei] indicates that there is a flight from city from to city toi 
@@ -30,5 +34,59 @@ The optimal path with no stops from city 0 to 2 is marked in red and has cost 50
 */
 
 public class Q787CheapestFlightsWithinKStops {
-    
+
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int K) {
+        ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+        int m = flights.length;
+        for (int i = 0; i < m; i++) {
+            adj.get(flights[i][0]).add(new Pair(flights[i][1], flights[i][2]));
+        }
+
+        // Create a queue which stores the node and their distances from the
+        // source in the form of {stops, {node, dist}} with ‘stops’ indicating
+        // the no. of nodes between src and current node.
+        Queue<Tuple> q = new LinkedList<>();
+
+        q.add(new Tuple(0, src, 0));
+
+        // Distance array to store the updated distances from the source.
+        int[] dist = new int[n];
+        for (int i = 0; i < n; i++) {
+            dist[i] = (int) (1e9);
+        }
+        dist[src] = 0;
+
+        // Iterate through the graph using a queue like in Dijkstra with
+        // popping out the element with min stops first.
+        while (!q.isEmpty()) {
+            Tuple it = q.peek();
+            q.remove();
+            int stops = it.first;
+            int node = it.second;
+            int cost = it.third;
+
+            // We stop the process as soon as the limit for the stops reaches.
+            if (stops > K)
+                continue;
+            for (Pair iter : adj.get(node)) {
+                int adjNode = iter.first;
+                int edW = iter.second;
+
+                // We only update the queue if the new calculated dist is
+                // less than the prev and the stops are also within limits.
+                if (cost + edW < dist[adjNode] && stops <= K) {
+                    dist[adjNode] = cost + edW;
+                    q.add(new Tuple(stops + 1, adjNode, cost + edW));
+                }
+            }
+        }
+        // If the destination node is unreachable return ‘-1’
+        // else return the calculated dist from src to dst.
+        if (dist[dst] == (int) (1e9))
+            return -1;
+        return dist[dst];
+    }
 }

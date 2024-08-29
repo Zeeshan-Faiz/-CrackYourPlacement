cc0f98700502fad3c143a2c01304aea48ec1f2e1
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-29 19:59:16 +0530

    Added QTopologicalSort code

--- Graph/MediumQuestions/QTopologicalSort.java
+++ Graph/MediumQuestions/QTopologicalSort.java
@@ -1,5 +1,15 @@
 package Graph.MediumQuestions;
 
+/*
+Given an adjacency list for a Directed Acyclic Graph (DAG) where adj_list[i] contains a list of 
+all vertices j such that there is a directed edge from vertex i to vertex j, with  V  vertices and 
+E  edges, your task is to find any valid topological sorting of the graph.
+In a topological sort, for every directed edge u -> v,  u must come before v in the ordering.
+
+Example 1:
+
+*/
+
 public class QTopologicalSort {
     
 }

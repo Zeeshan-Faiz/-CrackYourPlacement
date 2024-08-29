47d77de0c850c689b2b6839c6c3184cea3c61ee2
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-29 19:37:10 +0530

    Added QBellmanFordAlgorithm code

--- Graph/MediumQuestions/QBellmanFordAlgorithm.java
+++ Graph/MediumQuestions/QBellmanFordAlgorithm.java
@@ -1,5 +1,15 @@
 package Graph.MediumQuestions;
 
+/*
+Given a weighted and directed graph of V vertices and E edges, Find the shortest distance of all 
+the vertex's from the source vertex S. If a vertices can't be reach from the S then mark the 
+distance as 10^8. Note: If the Graph contains a negative cycle then return an array consisting of 
+only -1.
+
+Example 1:
+
+*/
+
 public class QBellmanFordAlgorithm {
     
 }

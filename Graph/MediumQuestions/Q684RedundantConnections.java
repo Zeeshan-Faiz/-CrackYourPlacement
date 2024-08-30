f6a4574850582dc97183d80a05ac394707a2b382
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-30 20:16:10 +0530

    Added Q684RedundantConnections code

--- Graph/MediumQuestions/Q684RedundantConnections.java
+++ Graph/MediumQuestions/Q684RedundantConnections.java
@@ -1,5 +1,10 @@
 package Graph.MediumQuestions;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 /*
 In this problem, a tree is an undirected graph that is connected and has no cycles.
 You are given a graph that started as a tree with n nodes labeled from 1 to n, with one additional 
@@ -20,4 +25,36 @@ Output: [1,4]
 
 public class Q684RedundantConnections {
     
+    public int[] findRedundantConnection(int[][] edges) {
+        int m = edges.length;
+
+        //we'll use hashmap to store our adjacency matrix
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+        for (int i = 1; i <= m; i++) {
+            map.put(i, new HashSet<>());
+        }
+
+        for (int[] edge : edges) {
+            if (dfs(new HashSet<>(), map, edge[0], edge[1]))
+                return edge;
+            map.get(edge[0]).add(edge[1]);
+            map.get(edge[1]).add(edge[0]);
+        }
+
+        return null;
+    }
+
+    private boolean dfs(Set<Integer> visited, Map<Integer, Set<Integer>> map,
+            int src, int target) {
+        if (src == target)
+            return true;
+        visited.add(src);
+        for (int next : map.get(src)) {
+            if (!visited.contains(next)) {
+                if (dfs(visited, map, next, target))
+                    return true;
+            }
+        }
+        return false;
+    }
 }

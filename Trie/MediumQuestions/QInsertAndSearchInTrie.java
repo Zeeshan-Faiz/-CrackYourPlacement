09686e87a71d06844ced0306776fb619416a505c
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-31 15:33:17 +0530

    Added QInsertAndSearchInTrie code

--- Trie/MediumQuestions/QInsertAndSearchInTrie.java
+++ Trie/MediumQuestions/QInsertAndSearchInTrie.java
@@ -1,5 +1,18 @@
 package Trie.MediumQuestions;
 
+/*
+Complete the Insert and Search functions for a Trie Data Structure. 
+    Insert: Accepts the Trie's root and a string, modifies the root in-place, and returns nothing.
+    Search: Takes the Trie's root and a string, returns true if the string is in the Trie, otherwise false.
+
+Note: To test the correctness of your code, the code-judge will be inserting a list of N strings 
+called into the Trie, and then will search for the string key in the Trie. The code-judge will 
+generate 1 if the key is present in the Trie, else 0.
+
+Example 1:
+
+*/
+
 public class QInsertAndSearchInTrie {
     
 }

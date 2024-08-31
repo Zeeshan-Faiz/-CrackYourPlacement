ceed99957e0ccf8af76e84c7d20508115cf852c0
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-31 15:33:40 +0530

    Added QInsertAndSearchInTrie code

--- Trie/MediumQuestions/QInsertAndSearchInTrie.java
+++ Trie/MediumQuestions/QInsertAndSearchInTrie.java
@@ -10,7 +10,20 @@ called into the Trie, and then will search for the string key in the Trie. The c
 generate 1 if the key is present in the Trie, else 0.
 
 Example 1:
+Input:
+n = 8
+list[] = {the, a, there, answer, any, by, bye, their}
+key = the
+Output: 1
+Explanation: "the" is present in the given set of strings. 
 
+Example 2:
+Input:
+n = 8
+list[] = {the, a, there, answer, any, by, bye, their}
+key = geeks
+Output: 0
+Explanation: "geeks" is not present in the given set of strings.
 */
 
 public class QInsertAndSearchInTrie {

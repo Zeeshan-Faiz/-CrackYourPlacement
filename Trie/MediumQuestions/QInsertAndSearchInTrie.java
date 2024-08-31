f4e55c0ab6efdee5f10bbb62e9a1eb7d913583fd
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-08-31 15:34:46 +0530

    Added QInsertAndSearchInTrie code

--- Trie/MediumQuestions/QInsertAndSearchInTrie.java
+++ Trie/MediumQuestions/QInsertAndSearchInTrie.java
@@ -28,8 +28,21 @@ Explanation: "geeks" is not present in the given set of strings.
 
 public class QInsertAndSearchInTrie {
 
+    static final int ALPHABET_SIZE = 26;
+
+    static class TrieNode {
+        TrieNode[] children = new TrieNode[ALPHABET_SIZE];
+        boolean isEndOfWord;
+
+        TrieNode() {
+            isEndOfWord = false;
+            for (int i = 0; i < ALPHABET_SIZE; i++)
+                children[i] = null;
+        }
+    }
+
     static void insert(TrieNode root, String key) {
-        // Your code here
+
         TrieNode curNode = root;
         for (int i = 0; i < key.length(); i++) {
             char ch = key.charAt(i);
@@ -43,9 +56,8 @@ public class QInsertAndSearchInTrie {
         curNode.isEndOfWord = true;
     }
 
-    // Function to use TRIE data structure and search the given string.
     static boolean search(TrieNode root, String key) {
-        // Your code here
+
         TrieNode curNode = root;
         for (int i = 0; i < key.length(); i++) {
             char ch = key.charAt(i);

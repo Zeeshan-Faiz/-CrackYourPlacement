d3622b31a6bc2e0e9068f9170d0446ef551f566e
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2025-01-04 11:04:17 +0530

    Added Q493ReversePairs code

---
+++ Array/HardQuestions/Q493ReversePairs.java
@@ -0,0 +1,17 @@
+package Array.HardQuestions;
+
+/*
+Given an integer array nums, return the number of reverse pairs in the array.
+
+A reverse pair is a pair (i, j) where:
+
+    0 <= i < j < nums.length and
+    nums[i] > 2 * nums[j].
+
+Example 1:
+
+*/
+
+public class Q493ReversePairs{
+
+}

a9692edc7f1c68cefabe03717d2abeca41429a47
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2025-01-07 23:22:12 +0530

    Added Q84LargestRectangleInHistogram code

--- Array/HardQuestions/Q84LargestRectangleInHistogram.java
+++ Array/HardQuestions/Q84LargestRectangleInHistogram.java
@@ -1,5 +1,7 @@
 package Array.HardQuestions;
 
+import java.util.Stack;
+
 /*
 Given an array of integers heights representing the histogram's bar height where the width of each 
 bar is 1, return the area of the largest rectangle in the histogram.
@@ -16,5 +18,38 @@ Output: 4
 */
 
 public class Q84LargestRectangleInHistogram {
-    
+
+    public int largestRectangleArea(int[] heights) {
+
+        Stack<Integer> stack = new Stack<>();
+        int max = 0;
+
+        stack.push(0);
+
+        for (int i = 1; i < heights.length; i++) {
+            while (!stack.isEmpty() && heights[i] < heights[stack.peek()]) {
+                max = getMax(heights, stack, max, i);
+            }
+            stack.push(i);
+        }
+
+        int i = heights.length;
+        while (!stack.isEmpty()) {
+            max = getMax(heights, stack, max, i);
+        }
+
+        return max;
+    }
+
+    private static int getMax(int[] arr, Stack<Integer> stack, int max, int i) {
+        
+        int area;
+        int popped = stack.pop();
+        if (stack.isEmpty()) {
+            area = arr[popped] * i;
+        } else {
+            area = arr[popped] * (i - 1 - stack.peek());
+        }
+        return Math.max(max, area);
+    }
 }

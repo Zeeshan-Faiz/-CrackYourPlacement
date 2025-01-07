8a5863b397255c5c569d5d37e42a32fcf80a3a03
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2025-01-07 23:21:29 +0530

    Added Q84LargestRectangleInHistogram code

--- Array/HardQuestions/Q84LargestRectangleInHistogram.java
+++ Array/HardQuestions/Q84LargestRectangleInHistogram.java
@@ -1,5 +1,13 @@
 package Array.HardQuestions;
 
+/*
+Given an array of integers heights representing the histogram's bar height where the width of each 
+bar is 1, return the area of the largest rectangle in the histogram.
+
+Example 1:
+
+*/
+
 public class Q84LargestRectangleInHistogram {
     
 }
